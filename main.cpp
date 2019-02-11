9cc569ddba5f7b6299e5e002e4ee27e478bde94c
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-11 15:31:44 -0500

    simple main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+#include "Sphere_t.h"
+#include "TransformMatrix_t.h"
+#include "Ray_t.h"
+#include "Material_t.h"
+
 int main(){
+    Material_t* material = new Material_t();
+    Ray_t* ray = new Ray_t();
+    TransformMatrix_t* transform_matrix = new TransformMatrix_t();
+
+    Sphere_t* sphere = new Sphere_t(material, transform_matrix);
     return 0;
 }
