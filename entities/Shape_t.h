867a5c8eb9476b2a1ddf1c8622ce4082abe544d6
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-11 15:50:45 -0500

    now works

--- entities/Shape_t.h
+++ entities/Shape_t.h
@@ -9,14 +9,14 @@
 class Shape_t{
     public:
         Shape_t(const Material_t *material, const TransformMatrix_t *transform_matrix): material_(material), transform_matrix_(transform_matrix){};
-        virtual ~Shape_t() = 0;
+        virtual ~Shape_t(){};
 
         virtual void update() = 0;
-        virtual void intersection(const Ray_t *ray, bool &intersected, double &t, double (&uv)[2])const  = 0; 
-        virtual void normaluv(const Ray_t *ray, const double (&uv)[2], double (&tuv)[2], Vec3f &normalvec)const = 0;
-        virtual void normal(const Ray_t *ray, const double (&uv)[2], Vec3f &normalvec)const = 0;
-        virtual void mincoord(Vec3f &coord)const = 0;
-        virtual void maxcoord(Vec3f &coord)const = 0;
+        virtual void intersection(const Ray_t *ray, bool &intersected, double &t, double (&uv)[2]) const  = 0; 
+        virtual void normaluv(const Ray_t *ray, const double (&uv)[2], double (&tuv)[2], Vec3f &normalvec) const = 0;
+        virtual void normal(const Ray_t *ray, const double (&uv)[2], Vec3f &normalvec) const = 0;
+        virtual void mincoord(Vec3f &coord) const = 0;
+        virtual void maxcoord(Vec3f &coord) const = 0;
 
         const Material_t *material_;
         const TransformMatrix_t *transform_matrix_;
--- shapes/Sphere_t.h
+++ shapes/Sphere_t.h
@@ -10,13 +10,13 @@
 class Sphere_t : public Shape_t{
     public:
         Sphere_t(const Material_t *material, const TransformMatrix_t *transform_matrix): Shape_t(material, transform_matrix){};
-        virtual ~Sphere_t();
+        virtual ~Sphere_t(){};
 
-        virtual void update();
-        virtual void intersection(const Ray_t *ray, bool &intersected, double &t, double (&uv)[2]) const; 
-        virtual void normaluv(const Ray_t *ray, const double (&uv)[2], double (&tuv)[2], Vec3f &normalvec) const;
-        virtual void normal(const Ray_t *ray, const double (&uv)[2], Vec3f &normalvec) const;
-        virtual void mincoord(Vec3f &coord) const;
-        virtual void maxcoord(Vec3f &coord) const;
+        virtual void update(){};
+        virtual void intersection(const Ray_t *ray, bool &intersected, double &t, double (&uv)[2]) const {}; 
+        virtual void normaluv(const Ray_t *ray, const double (&uv)[2], double (&tuv)[2], Vec3f &normalvec) const {};
+        virtual void normal(const Ray_t *ray, const double (&uv)[2], Vec3f &normalvec) const {};
+        virtual void mincoord(Vec3f &coord) const {};
+        virtual void maxcoord(Vec3f &coord) const {};
 };
 #endif
