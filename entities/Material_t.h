e0c01105f9050042cec03a25248c50851867e645
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-20 21:12:17 -0500

    material core function

--- entities/Material_t.h
+++ entities/Material_t.h
@@ -1,9 +1,15 @@
 #ifndef MATERIAL_T_H
 #define MATERIAL_T_H
 
+#include "Shape_t.h"
+#include "Ray_t.h"
+
 class Material_t{
     public:
         Material_t(){};
+        ~Material_t();
+
+        virtual void bounce(const double (&uv)[2], const Shape_t* hit_obj, Ray_t &ray) const = 0;
 };
 
 #endif
