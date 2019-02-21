d8ea368b7cad0ad6fcfa87be3f063f6337624ad8
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-20 21:14:48 -0500

    added members

--- entities/Ray_t.h
+++ entities/Ray_t.h
@@ -1,9 +1,22 @@
 #ifndef RAY_T_H
 #define RAY_T_H
 
+#include "Vec3f.h"
+#include "Medium_t.h"
+#include <list>
+
 class Ray_t{
     public:
         Ray_t(){};
+        ~Ray_t();
+
+        Vec3f origin_;
+        Vec3f direction_;
+        Vec3f mask_;
+        Vec3f colour_;
+        double dist_;
+        std::list <Medium_t> medium_list_;
+        double time_[2];
 };
 
 #endif
