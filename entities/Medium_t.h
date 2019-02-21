0f1d7f0382298d5719d9115333f8a46252c6a607
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-20 21:13:51 -0500

    added medium and scattering function

---
+++ entities/Medium_t.h
@@ -0,0 +1,17 @@
+#ifndef MEDIUM_T_H
+#define MEDIUM_T_H
+
+#include "Material_t.h"
+#include "Ray_t.h"
+#include "ScatteringFunction_t.h"
+
+class Medium_t : public Material_t{
+    public:
+        Medium_t(unsigned int priority, ScatteringFunction_t* scattering): priority_(priority), scattering_(scattering) {};
+        virtual ~Medium_t();
+
+        unsigned int priority_;
+        ScatteringFunction_t* scattering_;
+};
+
+#endif
---
+++ entities/ScatteringFunction_t.h
@@ -0,0 +1,14 @@
+#ifndef SCATTERINGFUNCTION_T_H
+#define SCATTERINGFUNCTION_T_H
+
+#include "Ray_t.h"
+
+class ScatteringFunction_t{
+    public:
+        ScatteringFunction_t(){};
+        ~ScatteringFunction_t();
+
+        virtual void scatter(Ray_t &ray, bool &intersected) const = 0; 
+};
+
+#endif
