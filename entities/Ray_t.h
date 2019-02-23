8ef880f3aba5ef128a755dc1dc9f69f79d3c3801
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-23 14:46:32 -0500

    added ray functions

--- entities/Ray_t.h
+++ entities/Ray_t.h
@@ -4,6 +4,8 @@
 #include "Vec3f.h"
 #include "Medium_t.h"
 #include <list>
+#include "Scene_t.h"
+#include "Skybox_t.h"
 
 class Ray_t{
     public:
@@ -17,6 +19,10 @@ class Ray_t{
         double dist_;
         std::list <Medium_t> medium_list_;
         double time_[2];
+
+        void raycast(Scene_t scene, unsigned int max_bounces, Skybox_t skybox);
+        void add_to_mediums(Medium_t medium);
+        void remove_from_mediums(Medium_t medium);
 };
 
 #endif
