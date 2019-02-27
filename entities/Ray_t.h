60754fddb167fce6424a363e7d92928d1ce52cec
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-27 14:50:39 -0500

    pointers

--- entities/Ray_t.h
+++ entities/Ray_t.h
@@ -9,7 +9,7 @@
 
 class Ray_t{
     public:
-        Ray_t(Vec3f origin, Vec3f direction, Vec3f colour, Vec3f mask, std::list<Medium_t> medium_list, double time[2] = {1, 1}); // Make constructor take time as opt input
+        Ray_t(Vec3f origin, Vec3f direction, Vec3f colour, Vec3f mask, std::list<Medium_t*> medium_list, double time[2] = {1, 1}); // Make constructor take time as opt input
         ~Ray_t();
 
         Vec3f origin_;
@@ -17,12 +17,12 @@ class Ray_t{
         Vec3f mask_;
         Vec3f colour_;
         double dist_;
-        std::list <Medium_t> medium_list_;
+        std::list <Medium_t*> medium_list_;
         double time_[2];
 
-        void raycast(Scene_t scene, unsigned int max_bounces, Skybox_t skybox);
-        void add_to_mediums(Medium_t medium);
-        void remove_from_mediums(Medium_t medium);
+        void raycast(Scene_t* scene, unsigned int max_bounces, Skybox_t* skybox);
+        void add_to_mediums(Medium_t* medium);
+        void remove_from_mediums(Medium_t* medium);
 };
 
 #endif
