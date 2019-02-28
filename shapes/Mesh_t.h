63f199dc7912a9dc0992ebabe80ed3bfdfe4e598
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-02-27 20:38:49 -0500

    forward declkarations and return types

--- shapes/Mesh_t.h
+++ shapes/Mesh_t.h
@@ -1,28 +1,32 @@
-#include "Material_t.h"
-#include "TransformMatrix_t.h"
-#include "Ray_t.h"
-#include "Vec3f.h"
-#include "Shape_t.h"
-#include "MeshGeometry_t.h"
-
 #ifndef MESH_T_H
 #define MESH_T_H
 
+//#include "Material_t.h"
+//#include "TransformMatrix_t.h"
+#include "Ray_t.h"
+#include "Vec3f.h"
+#include "Shape_t.h"
+//#include "MeshGeometry_t.h"
+
+class Material_t;
+class TransformMatrix_t;
+class MeshGeometry_t;
+
 class Mesh_t : public Shape_t{
     public:
-        Mesh_t(const Material_t *material, const TransformMatrix_t *transform_matrix): Shape_t(material, transform_matrix){};
+        Mesh_t(Material_t *material, TransformMatrix_t *transform_matrix): Shape_t(material, transform_matrix){};
         virtual ~Mesh_t(){};
 
         MeshGeometry_t* geom_;
         unsigned int n_tris_;
-        Shape_t** triangled_; // Maybe should be triangle**?
+        Shape_t** triangles_; // Maybe should be triangle**?
 
         virtual void update(){};
-        virtual void intersection(const Ray_t *ray, bool &intersected, double &t, double (&uv)[2]) const {}; // Never use this
-        virtual void normaluv(const Ray_t *ray, const double (&uv)[2], double (&tuv)[2], Vec3f &normalvec) const {};
-        virtual void normal(const Ray_t *ray, const double (&uv)[2], Vec3f &normalvec) const {};
-        virtual void mincoord(Vec3f &coord) const {};
-        virtual void maxcoord(Vec3f &coord) const {};
+        virtual void intersection(const Ray_t &ray, bool &intersected, double &t, double (&uv)[2]) const {}; // Never use this
+        virtual void normaluv(const Ray_t &ray, const double (&uv)[2], double (&tuv)[2], Vec3f &normalvec) const {};
+        virtual void normal(const Ray_t &ray, const double (&uv)[2], Vec3f &normalvec) const {};
+        virtual Vec3f mincoord() const {};
+        virtual Vec3f maxcoord() const {};
         virtual void createTriangles(){};
 };
 #endif
--- shapes/Sphere_t.h
+++ shapes/Sphere_t.h
@@ -1,22 +1,25 @@
-#include "Material_t.h"
-#include "TransformMatrix_t.h"
+#ifndef SPHERE_T_H
+#define SPHERE_T_H
+
+//#include "Material_t.h"
+//#include "TransformMatrix_t.h"
 #include "Ray_t.h"
 #include "Vec3f.h"
 #include "Shape_t.h"
 
-#ifndef SPHERE_T_H
-#define SPHERE_T_H
+class TransformMatrix_t;
+class Material_t;
 
 class Sphere_t : public Shape_t{
     public:
-        Sphere_t(const Material_t *material, const TransformMatrix_t *transform_matrix): Shape_t(material, transform_matrix){};
+        Sphere_t(Material_t *material, TransformMatrix_t *transform_matrix): Shape_t(material, transform_matrix){};
         virtual ~Sphere_t(){};
 
         virtual void update(){};
-        virtual void intersection(const Ray_t *ray, bool &intersected, double &t, double (&uv)[2]) const {}; 
-        virtual void normaluv(const Ray_t *ray, const double (&uv)[2], double (&tuv)[2], Vec3f &normalvec) const {};
-        virtual void normal(const Ray_t *ray, const double (&uv)[2], Vec3f &normalvec) const {};
-        virtual void mincoord(Vec3f &coord) const {};
-        virtual void maxcoord(Vec3f &coord) const {};
+        virtual void intersection(const Ray_t &ray, bool &intersected, double &t, double (&uv)[2]) const {}; 
+        virtual void normaluv(const Ray_t &ray, const double (&uv)[2], double (&tuv)[2], Vec3f &normalvec) const {};
+        virtual void normal(const Ray_t &ray, const double (&uv)[2], Vec3f &normalvec) const {};
+        virtual Vec3f mincoord() const {};
+        virtual Vec3f maxcoord() const {};
 };
 #endif
