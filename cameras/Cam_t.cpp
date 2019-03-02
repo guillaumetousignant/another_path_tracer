1b3f450c0c9bccd9cf052d1fb04b700a73c2a218
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-02 18:12:28 -0500

    added constructor

--- cameras/Cam_t.cpp
+++ cameras/Cam_t.cpp
@@ -5,13 +5,9 @@
 #include "Medium_t.h"
 #include "ImgBuffer_t.h"
 
-Cam_t::Cam_t(TransformMatrix_t* transformation, std::string filename, Vec3f up, double fov[2], unsigned int subpix[2], ImgBuffer_t* image, std::list<Medium_t*> medium_list, Skybox_t* skybox, unsigned int max_bounces, double gammaind) {
+Cam_t::Cam_t(TransformMatrix_t* transformation, std::string filename, Vec3f up, double fov[2], unsigned int subpix[2], ImgBuffer_t* image, std::list<Medium_t*> medium_list, Skybox_t* skybox, unsigned int max_bounces, double gammaind) : Camera_t(transformation, filename, up, fov, subpix, medium_list, skybox, max_bounces, gammaind), image_(image) {}
 
-}
-
-Cam_t::~Cam_t() {
-
-}
+Cam_t::~Cam_t() {}
 
 void Cam_t::update() {
 
@@ -25,7 +21,7 @@ void Cam_t::focus(double focus_distance) {
 
 }
 
-void Cam_t::write(std::string file_name = "") {
+void Cam_t::write(std::string file_name /*= ""*/) {
 
 }
 
--- cameras/Cam_t.h
+++ cameras/Cam_t.h
@@ -16,6 +16,8 @@ class Cam_t : public Camera_t{
         Cam_t(TransformMatrix_t* transformation, std::string filename, Vec3f up, double fov[2], unsigned int subpix[2], ImgBuffer_t* image, std::list<Medium_t*> medium_list, Skybox_t* skybox, unsigned int max_bounces, double gammaind);
         ~Cam_t();
 
+        ImgBuffer_t* image_;
+
         virtual void update();
         virtual void raytrace(Scene_t* scene) const;
         virtual void focus(double focus_distance);
