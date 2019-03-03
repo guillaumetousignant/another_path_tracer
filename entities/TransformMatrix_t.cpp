5f02d6c0b0cb145bcaf1f2bd093f2bfff2cbf307
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-02 22:51:03 -0500

    split into files

---
+++ entities/TransformMatrix_t.cpp
@@ -0,0 +1,93 @@
+#include "TransformMatrix_t.h"
+
+TransformMatrix_t::TransformMatrix_t(){
+
+}
+
+TransformMatrix_t::~TransformMatrix_t(){
+
+}
+
+void TransformMatrix_t::rotateXAxis(double angle){
+
+}
+
+void TransformMatrix_t::rotateYAxis(double angle){
+
+}
+
+void TransformMatrix_t::rotateZAxis(double angle){
+
+}
+
+void TransformMatrix_t::rotateX(double angle){
+
+}
+
+void TransformMatrix_t::rotateY(double angle){
+
+}
+
+void TransformMatrix_t::rotateZ(double angle){
+
+}
+
+void TransformMatrix_t::rotateAxis(Vec3f vec, double angle){
+
+}
+
+void TransformMatrix_t::rotate(Vec3f vec, double angle){
+
+}
+
+void TransformMatrix_t::translate(Vec3f vec){
+
+}
+
+void TransformMatrix_t::scaleAxis(Vec3f vec){
+
+}
+
+void TransformMatrix_t::scaleAxis(double fac){
+
+}
+
+void TransformMatrix_t::scale(Vec3f vec){
+
+}
+
+void TransformMatrix_t::scale(double fac){
+
+}
+
+void TransformMatrix_t::reflect(Vec3f vec){
+
+}
+
+void TransformMatrix_t::shear(Vec3f vec){
+
+}
+
+void TransformMatrix_t::transpose(){
+
+}
+
+void TransformMatrix_t::invert(){
+
+}
+
+void TransformMatrix_t::neg(){
+
+}
+
+Vec3f TransformMatrix_t::multVec(Vec3f vec){
+    return Vec3f();
+}
+
+Vec3f TransformMatrix_t::multDir(Vec3f vec){
+    return Vec3f();
+}
+
+TransformMatrix_t TransformMatrix_t::transformDir(){
+    return TransformMatrix_t();
+}
--- entities/TransformMatrix_t.h
+++ entities/TransformMatrix_t.h
@@ -5,30 +5,30 @@
 
 class TransformMatrix_t{
     public:
-        TransformMatrix_t(){};
-        ~TransformMatrix_t(){};
+        TransformMatrix_t();
+        ~TransformMatrix_t();
 
-        void rotateXAxis(double angle){};
-        void rotateYAxis(double angle){};
-        void rotateZAxis(double angle){};
-        void rotateX(double angle){};
-        void rotateY(double angle){};
-        void rotateZ(double angle){};
-        void rotateAxis(Vec3f vec, double angle){};
-        void rotate(Vec3f vec, double angle){};
-        void translate(Vec3f vec){};
-        void scaleAxis(Vec3f vec){};
-        void scaleAxis(double fac){};
-        void scale(Vec3f vec){};
-        void scale(double fac){};
-        void reflect(Vec3f vec){};
-        void shear(Vec3f vec){};
-        void transpose(){};
-        void invert(){};
-        void neg(){};
-        Vec3f multVec(Vec3f vec){};
-        Vec3f multDir(Vec3f vec){};
-        TransformMatrix_t transformDir(){};
+        void rotateXAxis(double angle);
+        void rotateYAxis(double angle);
+        void rotateZAxis(double angle);
+        void rotateX(double angle);
+        void rotateY(double angle);
+        void rotateZ(double angle);
+        void rotateAxis(Vec3f vec, double angle);
+        void rotate(Vec3f vec, double angle);
+        void translate(Vec3f vec);
+        void scaleAxis(Vec3f vec);
+        void scaleAxis(double fac);
+        void scale(Vec3f vec);
+        void scale(double fac);
+        void reflect(Vec3f vec);
+        void shear(Vec3f vec);
+        void transpose();
+        void invert();
+        void neg();
+        Vec3f multVec(Vec3f vec);
+        Vec3f multDir(Vec3f vec);
+        TransformMatrix_t transformDir();
 };
 
 #endif
