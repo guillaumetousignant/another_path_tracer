5f234dde6413fb19291e52ac61a6bc3ae861f7c0
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-02 20:02:21 -0500

    added to_xyz_offset

--- functions/Referentials.h
+++ functions/Referentials.h
@@ -12,4 +12,8 @@ Vec3f to_sph(const Vec3f &xyz) {
 Vec3f to_xyz(const Vec3f &sph) {
     return Vec3f(sph[0]*sin(sph[1])*cos(sph[2]), sph[0]*sin(sph[1])*sin(sph[2]), sph[0]*cos(sph[1]));
 }
+
+Vec3f to_xyz_offset(const Vec3f &sph, const Vec3f &ref1, const Vec3f &ref2, const Vec3f &ref3) {
+    return ref1 * sph[0]*sin(sph[1])*cos(sph[2]) + ref2 * sph[0]*sin(sph[1])*sin(sph[2]) + ref3 * sph[0]*cos(sph[1]);
+}
 #endif
