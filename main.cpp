c3cd11e67f82c817b9bd8615723b85d7024118da
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-03 17:56:15 -0500

    added accumulate call

--- main.cpp
+++ main.cpp
@@ -40,6 +40,9 @@ int main(){
 
     Cam_t* cam = new Cam_t(transform, filename, Vec3f(0.0, 0.0, 1.0), fov, subpix, imgbuffer, medium_list, skybox, maxbounces, 1.0);
 
+    cam->accumulate(scene, 1);
+
+    cam->write();
 
     return 0;
 }
