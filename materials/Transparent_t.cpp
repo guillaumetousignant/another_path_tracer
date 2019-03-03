47c123fade68d19d471e87dde41339b35e412610
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-03 17:43:59 -0500

    added transparent

---
+++ materials/Transparent_t.cpp
@@ -0,0 +1,33 @@
+#include "Transparent_t.h"
+#include "ScatteringFunction_t.h"
+#include "Shape_t.h"
+
+#define EPSILON 0.00000001
+
+Transparent_t::Transparent_t(unsigned int priority, ScatteringFunction_t* scattering) : Medium_t(priority, scattering) {
+
+}
+
+Transparent_t::~Transparent_t() {
+
+}
+
+void Transparent_t::bounce(const double (&uv)[2], const Shape_t* hit_obj, Ray_t &ray){
+    Vec3f axis;
+    Vec3f normal;
+    Vec3f u, v;
+    Vec3f newdir;
+    double cosi;
+
+    hit_obj->normal(ray, uv, normal);
+    cosi = ray.direction_.dot(normal);
+
+    if (cosi < 0) {
+        ray.origin_ += ray.direction_ * ray.dist_ - normal * EPSILON;
+        ray.add_to_mediums(this);
+    }
+    else{
+        ray.origin_ += ray.direction_ * ray.dist_ + normal * EPSILON;
+        ray.remove_from_mediums(this);
+    }
+}
---
+++ materials/Transparent_t.h
@@ -0,0 +1,18 @@
+#ifndef TRANSPARENT_T_H
+#define TRANSPARENT_T_H
+
+#include "Medium_t.h"
+#include "Vec3f.h"
+#include "Ray_t.h"
+
+class ScatteringFunction_t;
+class Shape_t;
+
+class Transparent_t : public Medium_t {
+    public:
+        Transparent_t(unsigned int priority, ScatteringFunction_t* scattering);
+        ~Transparent_t();
+
+        virtual void bounce(const double (&uv)[2], const Shape_t* hit_obj, Ray_t &ray);
+};
+#endif
