590414706b1d04ae532e12cfd4d48b56a4f2a32f
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-04 13:37:59 -0500

    added correct init and destructor

--- skyboxes/SkyboxFlatSun_t.cpp
+++ skyboxes/SkyboxFlatSun_t.cpp
@@ -1,11 +1,23 @@
 #include "SkyboxFlatSun_t.h"
 #include "DirectionalLight_t.h"
 
-SkyboxFlatSun_t::SkyboxFlatSun_t(const Vec3f &background, DirectionalLight_t* light) : background_(background), lights_(&light), n_lights_(1) {} 
+SkyboxFlatSun_t::SkyboxFlatSun_t(const Vec3f &background, DirectionalLight_t* light) : background_(background), n_lights_(1) {
+    lights_ = new DirectionalLight_t*[n_lights_];
+    lights_[0] = light;
+} 
 
-SkyboxFlatSun_t::SkyboxFlatSun_t(const Vec3f &background, DirectionalLight_t** lights, unsigned int n_lights) : background_(background), lights_(lights), n_lights_(n_lights) {}
+SkyboxFlatSun_t::SkyboxFlatSun_t(const Vec3f &background, DirectionalLight_t** lights, unsigned int n_lights) : background_(background), n_lights_(n_lights) {
+    lights_ = new DirectionalLight_t*[n_lights_];
+    for (unsigned int i = 0; i < n_lights_; i++){
+        lights_[i] = lights[i];
+    }
+}
 
-SkyboxFlatSun_t::~SkyboxFlatSun_t() {}
+SkyboxFlatSun_t::~SkyboxFlatSun_t() {
+    if (lights_ != nullptr){
+        delete [] lights_;
+    }
+}
 
 Vec3f SkyboxFlatSun_t::get(const Vec3f &xyz) const {
     Vec3f colour = Vec3f();
