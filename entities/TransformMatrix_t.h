f3758c0d43d40c5db4e48d95dd75e57f2b29d7d5
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-05 00:10:11 -0500

    spacing

--- entities/TransformMatrix_t.h
+++ entities/TransformMatrix_t.h
@@ -13,24 +13,24 @@ class TransformMatrix_t{
 
         double matrix_[16];
 
-        TransformMatrix_t&  rotateXAxis(double angle);
-        TransformMatrix_t&  rotateYAxis(double angle);
-        TransformMatrix_t&  rotateZAxis(double angle);
-        TransformMatrix_t&  rotateX(double angle);
-        TransformMatrix_t&  rotateY(double angle);
-        TransformMatrix_t&  rotateZ(double angle);
-        TransformMatrix_t&  rotateAxis(const Vec3f &vec, double angle);
-        TransformMatrix_t&  rotate(const Vec3f &vec, double angle);
-        TransformMatrix_t&  translate(const Vec3f &vec);
-        TransformMatrix_t&  scaleAxis(const Vec3f &vec);
-        TransformMatrix_t&  scaleAxis(double fac);
-        TransformMatrix_t&  scale(const Vec3f &vec);
-        TransformMatrix_t&  scale(double fac);
-        TransformMatrix_t&  reflect(const Vec3f &vec);
-        TransformMatrix_t&  shear(const Vec3f &vec);
+        TransformMatrix_t& rotateXAxis(double angle);
+        TransformMatrix_t& rotateYAxis(double angle);
+        TransformMatrix_t& rotateZAxis(double angle);
+        TransformMatrix_t& rotateX(double angle);
+        TransformMatrix_t& rotateY(double angle);
+        TransformMatrix_t& rotateZ(double angle);
+        TransformMatrix_t& rotateAxis(const Vec3f &vec, double angle);
+        TransformMatrix_t& rotate(const Vec3f &vec, double angle);
+        TransformMatrix_t& translate(const Vec3f &vec);
+        TransformMatrix_t& scaleAxis(const Vec3f &vec);
+        TransformMatrix_t& scaleAxis(double fac);
+        TransformMatrix_t& scale(const Vec3f &vec);
+        TransformMatrix_t& scale(double fac);
+        TransformMatrix_t& reflect(const Vec3f &vec);
+        TransformMatrix_t& shear(const Vec3f &vec);
         TransformMatrix_t& transpose();
         TransformMatrix_t& invert();
-        TransformMatrix_t&  neg();
+        TransformMatrix_t& neg();
         Vec3f multVec(const Vec3f &vec) const;
         Vec3f multDir(const Vec3f &vec) const;
         TransformMatrix_t transformDir() const; // Should return reference?
