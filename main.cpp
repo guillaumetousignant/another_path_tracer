47c4aabfbc39a2d91a1300fa28230267e7334eaa
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-04 21:11:48 -0500

    100

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ int main(){
 
     Cam_t* cam = new Cam_t(transform, filename, Vec3f(0.0, 0.0, 1.0), fov, subpix, imgbuffer, medium_list, skybox, maxbounces, 1.0);
 
-    cam->accumulate(scene, 1000);
+    cam->accumulate(scene, 100);
     cam->write();
 
     /*for (unsigned int i = 0; i < 1; i++){
