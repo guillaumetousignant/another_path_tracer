1af791cb7396fbc1d25d80d9a5d33c7072baa681
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-05 17:35:55 -0500

    added medium list functions

--- entities/Ray_t.cpp
+++ entities/Ray_t.cpp
@@ -35,9 +35,20 @@ void Ray_t::raycast(const Scene_t* scene, unsigned int max_bounces, const Skybox
 }
 
 void Ray_t::add_to_mediums(Medium_t* medium){
-
+    for (std::list<Medium_t*>::iterator i = medium_list_.begin(); i != medium_list_.end(); i++){
+        if ((*i)->priority_ <= medium->priority_){
+            medium_list_.insert(i, medium);
+            return;
+        }
+    }
+    medium_list_.push_back(medium);
 }
 
 void Ray_t::remove_from_mediums(Medium_t* medium){
-
+    for (std::list<Medium_t*>::iterator i = medium_list_.begin(); i != medium_list_.end(); i++){
+        if (*i == medium){
+            medium_list_.erase(i);
+            return;
+        }
+    }
 }
