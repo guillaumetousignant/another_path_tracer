fdf1ac79419be1069bac9210fb7326b2e67e4965
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-08 16:52:16 -0500

    new radius calculation

--- entities/DirectionalLight_t.cpp
+++ entities/DirectionalLight_t.cpp
@@ -4,7 +4,7 @@
 DirectionalLight_t::DirectionalLight_t(const Vec3f &intensity, TransformMatrix_t* transformation) : intensity_(intensity), transformation_(transformation) {
     TransformMatrix_t transform_norm = transformation_->transformDir();
     direction_ = transform_norm.multDir(Vec3f(0.0, 1.0, 0.0));
-    radius_ = Vec3f(transformation_->matrix_[0], transformation_->matrix_[5], transformation_->matrix_[10]).magnitude();
+    radius_ = (transformation_->matrix_[0] + transformation_->matrix_[5] + transformation_->matrix_[10])/3; 
 }
 
 DirectionalLight_t::~DirectionalLight_t(){}
@@ -12,5 +12,5 @@ DirectionalLight_t::~DirectionalLight_t(){}
 void DirectionalLight_t::update(){
     TransformMatrix_t transform_norm = transformation_->transformDir();
     direction_ = transform_norm.multDir(Vec3f(0.0, 1.0, 0.0));
-    radius_ = Vec3f(transformation_->matrix_[0], transformation_->matrix_[5], transformation_->matrix_[10]).magnitude();
+    radius_ = (transformation_->matrix_[0] + transformation_->matrix_[5] + transformation_->matrix_[10])/3;
 }
