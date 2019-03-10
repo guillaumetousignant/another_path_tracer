ce0121fc176fa1b9fe803903561db91ed06c1c4b
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-10 11:38:08 -0400

    material mix template, for scene read function

---
+++ entities/MaterialMix_t.h
@@ -0,0 +1,16 @@
+#ifndef MATERIALMIX_T_H
+#define MATERIALMIX_T_H
+
+#include "Material_t.h"
+
+class MaterialMix_t : public Material_t{
+    public:
+        MaterialMix_t(Material_t* material_refracted, Material_t* material_reflected) 
+            : material_refracted_(material_refracted), material_reflected_(material_reflected) {};
+        virtual ~MaterialMix_t(){};
+
+        Material_t* material_refracted_;
+        Material_t* material_reflected_;        
+};
+
+#endif
