83b1ba9ec1002e2d54a173d684a4b8d6f8470756
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-10 10:42:19 -0400

    added absorber

---
+++ materials/Absorber_t.h
@@ -0,0 +1,18 @@
+#ifndef ABSORBER_T_H
+#define ABSORBER_T_H
+
+#include "ScatteringFunction_t.h"
+#include "Ray_t.h"
+#include "Vec3f.h"
+
+class Absorber_t : public ScatteringFunction_t{
+    public:
+        Absorber_t(Vec3f emi_vol, Vec3f col_vol, double abs_dist_emi, double abs_dist_col);
+        ~Absorber_t();
+
+        Vec3f emission_vol_;
+        Vec3f colour_vol_;
+
+        virtual void scatter(Ray_t &ray, bool &intersected); 
+};
+#endif
