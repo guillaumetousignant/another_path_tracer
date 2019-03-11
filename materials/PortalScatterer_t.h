4e2ea0f5959e5417c575b605e437343a3ff4857f
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-11 15:53:13 -0400

    added scattering portal

---
+++ materials/PortalScatterer_t.h
@@ -0,0 +1,24 @@
+#ifndef PORTALSCATTERER_T_H
+#define PORTALSCATTERER_T_H
+
+#include "ScatteringFunction_t.h"
+#include "Ray_t.h"
+#include <list>
+#include <random>
+
+class TransformMatrix_t;
+class Medium_t;
+
+class PortalScatterer_t : public ScatteringFunction_t{
+    public:
+        PortalScatterer_t(TransformMatrix_t* transformation, double scattering_distance, std::list<Medium_t*> medium_list);
+        ~PortalScatterer_t();
+
+        TransformMatrix_t* transformation_;
+        std::list<Medium_t*> medium_list_;
+        double scattering_coefficient_;
+        std::uniform_real_distribution<double> unif_;
+
+        virtual void scatter(Ray_t &ray, bool &intersected); 
+};
+#endif
