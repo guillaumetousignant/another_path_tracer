cf00abbd11778ef0c1c0e6e7cce7cb629129dd78
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-11 16:11:31 -0400

    removed redundant line

--- materials/Reflective_t.cpp
+++ materials/Reflective_t.cpp
@@ -10,18 +10,15 @@ Reflective_t::~Reflective_t(){}
 
 void Reflective_t::bounce(const double (&uv)[2], const Shape_t* hit_obj, Ray_t &ray) {
     Vec3f normal;
-    Vec3f newdir;
 
     hit_obj->normal(ray, uv, normal);
 
     if (normal.dot(ray.direction_) > 0.0){
         normal *= -1.0;
     }
-
-    newdir = ray.direction_ - normal * 2.0 * ray.direction_.dot(normal);
     
     ray.origin_ += ray.direction_ * ray.dist_ + normal * EPSILON;
-    ray.direction_ = newdir;
+    ray.direction_ = ray.direction_ - normal * 2.0 * ray.direction_.dot(normal);
 
     ray.colour_ += ray.mask_ * emission_;
     ray.mask_ *= colour_;
