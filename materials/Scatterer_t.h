702ea98d7c77b162a042683e4da1a2eaac6c50a4
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-12 19:12:36 -0400

    added scatterer

---
+++ materials/Scatterer_t.h
@@ -0,0 +1,21 @@
+#ifndef SCATTERER_T_H
+#define SCATTERER_T_H
+
+#include "ScatteringFunction_t.h"
+#include "Ray_t.h"
+#include "Vec3f.h"
+#include <random>
+
+class Scatterer_t : public ScatteringFunction_t{
+    public:
+        Scatterer_t(Vec3f emi_vol, Vec3f col_vol, double abs_dist_emi, double abs_dist_col, double scat_dist);
+        ~Scatterer_t();
+
+        Vec3f emission_vol_;
+        Vec3f colour_vol_;
+        double scattering_coefficient_;
+        std::uniform_real_distribution<double> unif_;
+
+        virtual void scatter(Ray_t &ray, bool &intersected); 
+};
+#endif
