f7bd9eae555098386e1c9e423d23990ebc8cd32d
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-18 16:12:29 -0400

    filled triangle functions

--- shapes/Triangle_t.cpp
+++ shapes/Triangle_t.cpp
@@ -11,19 +11,72 @@
 
 Triangle_t::Triangle_t(Material_t *material, TransformMatrix_t *transform_matrix, Vec3f (&points)[3], Vec3f (&normals)[3], double (&texcoord)[3][2]) 
     : TriangleTop_t(material, transform_matrix), points_orig_{points[0], points[1], points[2]} {
-    v0v1_ = points[1] - points[0];
-    v0v2_ = points[2] - points[0];
-    
-    
 
+    if (normals == nullptr){
+        Vec3f nor = (points[1] - points[0]).cross(points[2] - points[0]).normalize(); 
+        // Loop or explicit?
+        for (unsigned int i = 0; i < 3; i++){
+            normals_orig_[i] = nor;
+        }
+    }
+    else{
+        for (unsigned int i = 0; i < 3; i++){
+            normals_orig_[i] = normals[i];
+        }
+    }
+
+    if (texcoord == nullptr){
+        for (unsigned int i = 0; i < 3; i++){
+            for (unsigned int j = 0; j < 2; j++){
+                texture_coordinates_[i][j] = 0;
+            }
+        }
+    }
+    else{
+        for (unsigned int i = 0; i < 3; i++){
+            for (unsigned int j = 0; j < 2; j++){
+                texture_coordinates_[i][j] = texcoord[i][j];
+            }
+        }
+    }
+
+    for (unsigned int i = 0; i < 3; i++){ // Loop or explicit?
+        points_[i] = transformation_->multVec(points_orig_[i]);
+    }
+
+    TransformMatrix_t transform_norm = transformation_->transformDir();
+
+    for (unsigned int i = 0; i < 3; i++){ // Loop or explicit?
+        normals_[i] = transformation_->multDir(normals_orig_[i]);
+    }
+
+    v0v1_ = points_[1] - points_[0];
+    v0v2_ = points_[2] - points_[0];
 }
 
 Triangle_t::~Triangle_t(){}
 
 void Triangle_t::update() {
+    for (unsigned int i = 0; i < 3; i++){ // Loop or explicit?
+        points_[i] = transformation_->multVec(points_orig_[i]);
+    }
 
+    TransformMatrix_t transform_norm = transformation_->transformDir();
+
+    for (unsigned int i = 0; i < 3; i++){ // Loop or explicit?
+        normals_[i] = transformation_->multDir(normals_orig_[i]);
+    }
+
+    v0v1_ = points_[1] - points_[0];
+    v0v2_ = points_[2] - points_[0];
 }
 
 void Triangle_t::normaluv(const Ray_t &ray, const double (&uv)[2], double (&tuv)[2], Vec3f &normalvec) const {
-
+    Vec3f distance = Vec3f(1.0 - uv[0] - uv[1], uv[0], uv[1]);
+    normalvec = Vec3f(distance[0] * normals_[0][0] + distance[1] * normals_[1][0] + distance[2] * normals_[2][0], 
+        distance[0] * normals_[0][1] + distance[1] * normals_[1][1] + distance[2] * normals_[2][1],
+        distance[0] * normals_[0][2] + distance[1] * normals_[1][2] + distance[2] * normals_[2][2]);
+    // Matrix multiplication, optimise.
+    tuv[0] = distance[0] * texture_coordinates_[0][0] + distance[1] * texture_coordinates_[1][0] + distance[2] * texture_coordinates_[2][0];
+    tuv[1] = distance[0] * texture_coordinates_[0][1] + distance[1] * texture_coordinates_[1][1] + distance[2] * texture_coordinates_[2][1];
 }
