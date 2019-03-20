ebc8273d0147ca378030d7405765f3f154580b8e
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-20 19:59:23 -0400

    added destructor

--- entities/MeshGeometry_t.cpp
+++ entities/MeshGeometry_t.cpp
@@ -5,5 +5,25 @@ MeshGeometry_t::MeshGeometry_t(){
 }
 
 MeshGeometry_t::~MeshGeometry_t(){
+    if (mat_ != nullptr){
+        delete [] mat_;
+    }
+
+    if (v_ != nullptr){
+        delete [] v_;
+    }
+
+    if (vn_ != nullptr){
+        delete [] vn_;
+    }
+
+    if (vt_ != nullptr){
+        for (unsigned int i = 0; i < n_tris_; i++){
+            if (vt_[i] != nullptr){
+                delete [] vt_[i];
+            }
+        }
+        delete [] vt_;
+    }
 
 }
