a57373a5a18a776278d3020d74a962f301c3bc53
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-03-24 14:46:00 -0400

    cells now reset hit obj to nullptr, else we'd get t=inf everywhere

--- acceleration/GridCell_t.cpp
+++ acceleration/GridCell_t.cpp
@@ -3,6 +3,8 @@
 #include <list>
 #include <limits>
 
+#include <iostream> // REMOVE
+
 GridCell_t::GridCell_t() : items_(std::list<Shape_t*>()) {}
 
 GridCell_t::~GridCell_t(){}
@@ -17,7 +19,8 @@ void GridCell_t::intersect(const Ray_t &ray, Shape_t* &hit_obj, double &t, doubl
     double t_temp;
     bool intersected;
     double uv_temp[2];
-    
+
+    hit_obj = nullptr; // dunno if this is needed    
     t = std::numeric_limits<double>::infinity();
 
     for (auto it = items_.begin(); it != items_.end(); ++it){
@@ -28,6 +31,12 @@ void GridCell_t::intersect(const Ray_t &ray, Shape_t* &hit_obj, double &t, doubl
             uv[1] = uv_temp[1];
             t = t_temp;
         }
+    }
+
+    if ((t == std::numeric_limits<double>::infinity()) && (hit_obj != nullptr)){
+        std::cout << "in gridcell EEEEEEEEEEEEEEEEEEEEE" << std::endl; // REMOVE
+        std::cout << "t_temp: " << t_temp << std::endl; // REMOVE
+        std::cout << "inf < inf = " << (std::numeric_limits<double>::infinity() < std::numeric_limits<double>::infinity()) << std::endl;
     }
 }
 
