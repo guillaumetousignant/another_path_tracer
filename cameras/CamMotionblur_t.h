cffb23a5f9cf63cc646c3d161c6be26cbd3ebb45
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-04-01 16:12:10 -0400

    addec cam motion blur

---
+++ cameras/CamMotionblur_t.h
@@ -0,0 +1,32 @@
+#ifndef CAMMOTIONBLUR_T_H
+#define CAMMOTIONBLUR_T_H
+
+#include "Cam_t.h"
+#include "Vec3f.h"
+#include <string>
+#include <list>
+#include <random>
+
+class TransformMatrix_t;
+class Skybox_t;
+class Scene_t;
+class Medium_t;
+class ImgBuffer_t;
+
+class CamMotionblur_t : public Cam_t{
+    public:
+        CamMotionblur_t(TransformMatrix_t* transformation, std::string filename, Vec3f up, const double (&fov)[2], const unsigned int (&subpix)[2], ImgBuffer_t* image, std::list<Medium_t*> &medium_list, Skybox_t* skybox, unsigned int max_bounces, double (&time)[2], double gammaind);
+        ~CamMotionblur_t();
+
+        Vec3f direction_last_;
+        Vec3f origin_last_;
+        double time_[2];
+        Vec3f up_last_;
+
+        virtual void update();
+        virtual void raytrace(const Scene_t* scene);
+        virtual void write(std::string file_name = "");
+        virtual void show() const;
+        virtual void reset();
+};
+#endif
