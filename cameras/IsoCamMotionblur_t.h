9d6f431f1afe0fa783cba4db96b6d056d20275ba
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-04-18 19:17:38 -0400

    added isocam motionblur

---
+++ cameras/IsoCamMotionblur_t.h
@@ -0,0 +1,29 @@
+#ifndef ISOCAMMOTIONBLUR_T_H
+#define ISOCAMMOTIONBLUR_T_H
+
+#include "IsoCam_t.h"
+#include "Vec3f.h"
+#include <string>
+#include <list>
+#include <random>
+
+class TransformMatrix_t;
+class Skybox_t;
+class Scene_t;
+class Medium_t;
+class ImgBuffer_t;
+
+class IsoCamMotionblur_t : public IsoCam_t{
+    public:
+        IsoCamMotionblur_t(TransformMatrix_t* transformation, std::string filename, Vec3f up, const double (&fov)[2], const unsigned int (&subpix)[2], ImgBuffer_t* image, std::list<Medium_t*> &medium_list, Skybox_t* skybox, unsigned int max_bounces, double (&time)[2], double gammaind);
+        ~IsoCamMotionblur_t();
+
+        Vec3f direction_last_;
+        Vec3f origin_last_;
+        double time_[2];
+        Vec3f up_last_;
+
+        virtual void update();
+        virtual void raytrace(const Scene_t* scene);
+};
+#endif
