a033df9dd91a6bfb672bbc6b5ecfd251f546a643
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-05-16 12:10:03 -0400

    added raytrace

--- cameras/Cam3D_t.cpp
+++ cameras/Cam3D_t.cpp
@@ -69,45 +69,8 @@ void Cam3D_t::update() {
 }
 
 void Cam3D_t::raytrace(const Scene_t* scene) {
-    double tot_subpix;
-    double pixel_span_x, pixel_span_y;
-    double subpix_span_x, subpix_span_y;
-    Vec3f horizontal, vertical;
-    
-    tot_subpix = subpix_[0]*subpix_[1];
-    pixel_span_y = fov_[0]/image_->size_y_;
-    pixel_span_x = fov_[1]/image_->size_x_;
-    subpix_span_y = pixel_span_y/subpix_[0];
-    subpix_span_x = pixel_span_x/subpix_[1];
-    horizontal = direction_.cross(up_).normalize();
-    vertical = horizontal.cross(direction_).normalize();
-
-    image_->update();
-
-    #pragma omp parallel for collapse(2) schedule(guided)
-    for (unsigned int j = 0; j < image_->size_y_; j++){
-        for (unsigned int i = 0; i < image_->size_x_; i++){
-            Vec3f col = Vec3f(); // Or declare above?
-            Vec3f pix_vec = Vec3f(1.0, PI/2.0 + ((double)j - (double)image_->size_y_/2.0 + 0.5)*pixel_span_y, ((double)i - (double)image_->size_x_/2.0 + 0.5)*pixel_span_x); // Is shit after this line
-            
-            for (unsigned int k = 0; k < subpix_[0]; k++){
-                for (unsigned int l = 0; l < subpix_[1]; l++){
-                    
-                    double jitter_y = unif_(my_rand::rng);
-                    double jitter_x = unif_(my_rand::rng);
-
-                    Vec3f subpix_vec = pix_vec + Vec3f(0.0, ((double)k - (double)subpix_[0]/2.0 + jitter_y)*subpix_span_y, ((double)l - (double)subpix_[1]/2.0 + jitter_x)*subpix_span_x); // Is shit after this line
-                    subpix_vec = to_xyz_offset(subpix_vec, direction_, horizontal, vertical);
-
-                    Ray_t ray = Ray_t(origin_, subpix_vec, Vec3f(), Vec3f(1.0, 1.0, 1.0), medium_list_);
-                    ray.raycast(scene, max_bounces_, skybox_);
-                    col += ray.colour_;
-                }
-            }
-            col = col/tot_subpix;
-            image_->update(col, i, j);
-        }
-    }
+    camera_L_->raytrace(scene);
+    camera_R_->raytrace(scene);
 }
 
 void Cam3D_t::write(std::string file_name /*= ""*/) {
