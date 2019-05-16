ba8b565d8c8597683c41f48a14cc890c3fcbd44c
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-05-16 12:08:09 -0400

    constructor and update done

--- cameras/Cam3D_t.cpp
+++ cameras/Cam3D_t.cpp
@@ -16,6 +16,7 @@ Cam3D_t::Cam3D_t(TransformMatrix_t* transformation, const std::string &filename,
 
     std::string filename_S, filename_L, filename_R;
     size_t point;
+    Vec3f horizontal;
 
     point = filename.find_last_of(".");
 
@@ -34,7 +35,13 @@ Cam3D_t::Cam3D_t(TransformMatrix_t* transformation, const std::string &filename,
     camera_R_ = new Cam_t(transformation, filename_R, up_, fov_, subpix_, image_R, medium_list_, skybox_, max_bounces_, gammaind_);
 
     filename_ = filename_S;
-    image_ = image;
+
+    horizontal = direction_.cross(up).normalize();
+
+    camera_L_->origin_ = horizontal * -eye_dist_/2.0 + origin_;
+    camera_R_->origin_ = horizontal * eye_dist_/2.0 + origin_;
+    camera_L_->direction_ = (direction_ * focal_length_ + horizontal * eye_dist_/2.0).normalize();
+    camera_R_->direction_ = (direction_ * focal_length_ - horizontal * eye_dist_/2.0).normalize();
 }
 
 Cam3D_t::~Cam3D_t() {
@@ -43,10 +50,22 @@ Cam3D_t::~Cam3D_t() {
 }
 
 void Cam3D_t::update() {
+    Vec3f horizontal;
+
     origin_ = transformation_->multVec(Vec3f(0.0, 0.0, 0.0));
     TransformMatrix_t transform_norm = transformation_->transformDir();
     direction_ = transform_norm.multDir(Vec3f(0.0, 1.0, 0.0));
+    focal_length_ = focal_length_buffer_;
     up_ = up_buffer_;
+
+    camera_L_->up_ = up_;
+    camera_R_->up_ = up_;
+
+    horizontal = direction_.cross(up_);
+    camera_L_->origin_ = horizontal * -eye_dist_/2.0 + origin_;
+    camera_R_->origin_ = horizontal * eye_dist_/2.0 + origin_;
+    camera_L_->direction_ = (direction_ * focal_length_ + horizontal * eye_dist_/2.0).normalize();
+    camera_R_->direction_ = (direction_ * focal_length_ - horizontal * eye_dist_/2.0).normalize();
 }
 
 void Cam3D_t::raytrace(const Scene_t* scene) {
