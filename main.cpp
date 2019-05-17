9202e0724ba8506ba6aaf96be2365580599c8a04
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-05-17 17:55:36 -0400

    added cam3dmotionbluraperture to main

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@
 #include "Cam3D_t.h"
 #include "Cam3DAperture_t.h"
 #include "Cam3DMotionblur_t.h"
+#include "Cam3DMotionblurAperture_t.h"
 #include <string>
 #include <list>
 #include "NonAbsorber_t.h"
@@ -467,7 +468,7 @@ int main(int argc, char **argv){
     std::list<Medium_t*> medium_list;
     medium_list.assign(2, air);
 
-    Cam3DMotionblur_t* cam = new Cam3DMotionblur_t(transform_camera, filename, Vec3f(0.0, 0.0, 1.0), fov, subpix, imgbuffer, imgbuffer_L, imgbuffer_R, eye_dist, medium_list, skybox, maxbounces, focal_length, time, 1.0);
+    Cam3DMotionblurAperture_t* cam = new Cam3DMotionblurAperture_t(transform_camera, filename, Vec3f(0.0, 0.0, 1.0), fov, subpix, imgbuffer, imgbuffer_L, imgbuffer_R, eye_dist, medium_list, skybox, maxbounces, focal_length, aperture, time, 1.0);
     thecamera = cam;
     cam->transformation_->translate(Vec3f(0.0, -2, 0.0));
     cam->update();
