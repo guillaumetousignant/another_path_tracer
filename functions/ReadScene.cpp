feb724837fcaa40692f3944907a7669b90d66edb
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-05-21 16:56:34 -0400

    added read_scene

---
+++ functions/ReadScene.cpp
@@ -0,0 +1,5 @@
+#include "ReadScene.h"
+
+void read_scene(const std::string &xml_filename){
+    
+}
---
+++ functions/ReadScene.h
@@ -0,0 +1,8 @@
+#ifndef READSCENE_H
+#define READSCENE_H
+
+#include <string>
+
+void read_scene(const std::string &xml_filename);
+
+#endif
