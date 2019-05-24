7b54c5aa08fadd348ec285937940bd359327c966
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-05-24 14:27:18 -0400

    created scenecontext

---
+++ entities/SceneContext_t.cpp
@@ -0,0 +1,146 @@
+#include "SceneContext_t.h"
+
+#include "Diffuse_t.h"
+#include "DiffuseTex_t.h"
+#include "Refractive_t.h"
+#include "Reflective_t.h"
+#include "ReflectiveRefractiveFuzz_t.h"
+#include "ReflectiveRefractive_t.h"
+
+#include "NonAbsorber_t.h"
+#include "Absorber_t.h"
+#include "Transparent_t.h"
+
+#include "TransformMatrix_t.h"
+#include "ImgBuffer_t.h"
+#include "ImgBufferOpenGL_t.h"
+#include "Scene_t.h"
+#include "MeshGeometry_t.h"
+#include "DirectionalLight_t.h"
+#include "Texture_t.h"
+
+#include "Sphere_t.h"
+#include "SphereMotionblur_t.h"
+#include "Triangle_t.h"
+#include "TriangleMotionblur_t.h"
+#include "Mesh_t.h"
+#include "MeshMotionblur_t.h"
+
+#include "Camera_t.h"
+#include "Cam_t.h"
+#include "CamAperture_t.h"
+#include "CamMotionblur_t.h"
+#include "CamMotionblurAperture_t.h"
+#include "RecCam_t.h"
+#include "RecCamAperture_t.h"
+#include "RecCamMotionblur_t.h"
+#include "RecCamMotionblurAperture_t.h"
+#include "IsoCam_t.h"
+#include "IsoCamAperture_t.h"
+#include "IsoCamMotionblur_t.h"
+#include "IsoCamMotionblurAperture_t.h"
+#include "Cam3D_t.h"
+#include "Cam3DAperture_t.h"
+#include "Cam3DMotionblur_t.h"
+#include "Cam3DMotionblurAperture_t.h"
+
+#include "SkyboxFlat_t.h"
+#include "SkyboxFlatSun_t.h"
+
+SceneContext_t::SceneContext_t() :
+    use_gl_(false) {}
+
+SceneContext_t::~SceneContext_t() {
+    reset();
+}
+
+void SceneContext_t::readXML(const std::string &filename){
+    reset();
+}    
+
+void SceneContext_t::reset(){
+    // Deleting buffers
+    if (transform_matrices_ != nullptr){
+        for (unsigned int i = 0; i < n_transform_matrices_; i++){
+            delete transform_matrices_[i];
+        }
+        delete [] transform_matrices_;
+    }
+    n_transform_matrices_ = 0;
+
+    if (textures_ != nullptr){
+        for (unsigned int i = 0; i < n_textures_; i++){
+            delete textures_[i];
+        }
+        delete [] textures_;
+    }
+    n_textures_ = 0;
+    
+    if (scatterers_ != nullptr){
+        for (unsigned int i = 0; i < n_scatterers_; i++){
+            delete scatterers_[i];
+        }
+        delete [] scatterers_;
+    }
+    n_scatterers_ = 0;
+
+    if (materials_ != nullptr){
+        for (unsigned int i = 0; i < n_materials_; i++){
+            delete materials_[i];
+        }
+        delete [] materials_;
+    }
+    n_materials_ = 0;
+
+    if (mesh_geometries_ != nullptr){
+        for (unsigned int i = 0; i < n_mesh_geometries_; i++){
+            delete mesh_geometries_[i];
+        }
+        delete [] mesh_geometries_;
+    }
+    n_mesh_geometries_ = 0;
+
+    if (objects_ != nullptr){
+        for (unsigned int i = 0; i < n_objects_; i++){
+            delete objects_[i];
+        }
+        delete [] objects_;
+    }
+    n_objects_ = 0;
+
+    if (directional_lights_ != nullptr){
+        for (unsigned int i = 0; i < n_directional_lights_; i++){
+            delete directional_lights_[i];
+        }
+        delete [] directional_lights_;
+    }
+    n_directional_lights_ = 0;
+
+    if (skyboxes_ != nullptr){
+        for (unsigned int i = 0; i < n_skyboxes_; i++){
+            delete skyboxes_[i];
+        }
+        delete [] skyboxes_;
+    }
+    n_skyboxes_ = 0;
+
+    if (imgbuffers_ != nullptr){
+        for (unsigned int i = 0; i < n_imgbuffers_; i++){
+            delete imgbuffers_[i];
+        }
+        delete [] imgbuffers_;
+    }
+    n_imgbuffers_ = 0;
+
+    if (cameras_ != nullptr){
+        for (unsigned int i = 0; i < n_cameras_; i++){
+            delete cameras_[i];
+        }
+        delete [] cameras_;
+    }
+    n_cameras_ = 0;
+}
+
+TransformMatrix_t* SceneContext_t::create_transform_matrix(const tinyxml2::XMLElement* xml_transform_matrix) const {
+
+}
---
+++ entities/SceneContext_t.h
@@ -0,0 +1,53 @@
+#ifndef SCENECONTEXT_T_H
+#define SCENECONTEXT_T_H
+
+#include <string>
+#include "tinyxml2.h"
+
+class TransformMatrix_t;
+class Texture_t;
+class ScatteringFunction_t;
+class Material_t;
+class MeshGeometry_t;
+class Shape_t;
+class DirectionalLight_t;
+class Skybox_t;
+class ImgBuffer_t;
+class Camera_t;
+
+class SceneContext_t{
+    public:
+        SceneContext_t();
+        ~SceneContext_t();
+
+        bool use_gl_;
+        std::string scene_name_;
+
+        unsigned int n_transform_matrices_;
+        unsigned int n_textures_;
+        unsigned int n_scatterers_;
+        unsigned int n_materials_;
+        unsigned int n_mesh_geometries_;
+        unsigned int n_objects_;
+        unsigned int n_directional_lights_;
+        unsigned int n_skyboxes_;
+        unsigned int n_imgbuffers_;
+        unsigned int n_cameras_;
+        
+        TransformMatrix_t** transform_matrices_;
+        Texture_t** textures_;
+        ScatteringFunction_t** scatterers_;
+        Material_t** materials_;
+        MeshGeometry_t** mesh_geometries_;
+        Shape_t** objects_;
+        DirectionalLight_t** directional_lights_;
+        Skybox_t** skyboxes_;
+        ImgBuffer_t** imgbuffers_;
+        Camera_t** cameras_;
+
+        void readXML(const std::string &filename);
+        void reset();
+        TransformMatrix_t* create_transform_matrix(const tinyxml2::XMLElement* xml_transform_matrix) const;
+};
+
+#endif
