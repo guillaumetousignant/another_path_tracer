49aa8d339cc7edc96789f8ae568963d0f7dd6405
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-07-31 10:19:46 -0400

    added prompt for scene path if none is given

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
+#include <string>
 #include "RandomGenerator_t.h"
 #include "SceneContext_t.h"
 
 int main(int argc, char **argv){
+    std::string filename = "";
     if (argc < 2){
-        std::cout << "Must specify scene filename. Exiting." << std::endl;
-        return 42;
+        std::cout << "Enter scene path:" << std::endl;
+        while (filename.empty()){
+            std::getline(std::cin, filename);
+        }
+        if (filename == "q"){
+            return -1;
+        }
+    }
+    else{
+        filename = argv[1];
     }
 
     SceneContext_t scene_context = SceneContext_t();
 
-    scene_context.readXML(argv[1]);
+    scene_context.readXML(filename);
     scene_context.render();
     
     return 0;
