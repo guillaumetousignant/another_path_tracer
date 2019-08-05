2ca6bdbe180b3411d7bf19b6ffc7396d46f69a65
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-08-05 10:09:50 -0400

    reduced inheritance

--- cameras/RecCamMotionblur_t.cpp
+++ cameras/RecCamMotionblur_t.cpp
@@ -10,8 +10,8 @@
 #define PI 3.141592653589793238463
 
 RecCamMotionblur_t::RecCamMotionblur_t(TransformMatrix_t* transformation, const std::string &filename, Vec3f up, const double (&fov)[2], const unsigned int (&subpix)[2], ImgBuffer_t* image, std::list<Medium_t*> medium_list, Skybox_t* skybox, unsigned int max_bounces, double (&time)[2], double gammaind) 
-    : RecCam_t(transformation, filename, up, fov, subpix, image, medium_list, skybox, max_bounces, gammaind), 
-    direction_last_(direction_), origin_last_(origin_), time_{time[0], time[1]}, up_last_(up_) {}
+    : Camera_t(transformation, filename, up, fov, subpix, medium_list, skybox, max_bounces, gammaind), 
+    image_(image), unif_(0.0, 1.0), direction_last_(direction_), origin_last_(origin_), time_{time[0], time[1]}, up_last_(up_) {}
 
 RecCamMotionblur_t::~RecCamMotionblur_t() {}
 
@@ -77,4 +77,19 @@ void RecCamMotionblur_t::raytrace(const Scene_t* scene) {
             image_->update(col, i, j);
         }
     }
+}
+
+void RecCamMotionblur_t::write(std::string file_name /*= ""*/) {
+    if (file_name.empty()){
+        file_name = filename_;
+    }
+    image_->write(file_name);
+}
+
+void RecCamMotionblur_t::show() const {
+    // What to do here?
+}
+
+void RecCamMotionblur_t::reset(){
+    image_->reset();
 }
