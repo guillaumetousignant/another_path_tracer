70756166b1d480216349662a3e6e3d78b1294181
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-08-07 10:11:48 -0400

    added final and non virtual func

--- entities/ImgBufferOpenGL_t.h
+++ entities/ImgBufferOpenGL_t.h
@@ -8,17 +8,17 @@
 #include "GL/glut.h"
 #include "GL/gl.h"
 
-class ImgBufferOpenGL_t : public ImgBuffer_t{
+class ImgBufferOpenGL_t final : public ImgBuffer_t{
     public:
         ImgBufferOpenGL_t(unsigned int size_x, unsigned int size_y);
-        virtual ~ImgBufferOpenGL_t();
+        virtual ~ImgBufferOpenGL_t() final;
 
-        virtual void reset();
-        virtual void update(const Vec3f** img, unsigned int size_x, unsigned int size_y);
-        virtual void update();
-        virtual void update(const Vec3f &colour, unsigned int pos_x, unsigned int pos_y);
-        virtual void set(const Vec3f** img, unsigned int size_x, unsigned int size_y);
-        virtual void set(const Vec3f &colour, unsigned int pos_x, unsigned int pos_y);
+        virtual void reset() final;
+        virtual void update(const Vec3f** img, unsigned int size_x, unsigned int size_y) final;
+        virtual void update() final;
+        virtual void update(const Vec3f &colour, unsigned int pos_x, unsigned int pos_y) final;
+        virtual void set(const Vec3f** img, unsigned int size_x, unsigned int size_y) final;
+        virtual void set(const Vec3f &colour, unsigned int pos_x, unsigned int pos_y) final;
 
         unsigned char* img_gl_;
         GLuint tex_;
