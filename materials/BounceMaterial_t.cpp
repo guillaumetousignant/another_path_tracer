b543fbff773d4e3d360bd9fdc44505897a27d947
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-08-08 10:09:56 -0400

    fixed bounce material

--- materials/BounceMaterial_t.cpp
+++ materials/BounceMaterial_t.cpp
@@ -1,14 +1,46 @@
 #include "BounceMaterial_t.h"
 #include "Shape_t.h"
 #include "Vec3f.h"
+#include <cmath>
+#include "RandomGenerator_t.h"
 
-BounceMaterial_t::BounceMaterial_t(unsigned int max_bounces) {
+#define EPSILON 0.00001 // was 0.00001, 0.01 works ok when *t
+#define PI 3.141592653589793238463
+
+BounceMaterial_t::BounceMaterial_t(unsigned int max_bounces) : unif_(std::uniform_real_distribution<double>(0, 1)) {
     emission_ = Vec3f(1.0/max_bounces);
 }
 
 BounceMaterial_t::~BounceMaterial_t(){}
 
 void BounceMaterial_t::bounce(const double (&uv)[2], const Shape_t* hit_obj, Ray_t &ray) {
+    Vec3f axis;
+    Vec3f normal;
+    Vec3f u, v;
+    Vec3f newdir;
+    double rand1, rand2, rand2s;
+
+    hit_obj->normal(ray, uv, normal);
+
+    rand1 = unif_(my_rand::rng)*2*PI;
+    rand2 = unif_(my_rand::rng);
+    rand2s = sqrt(rand2);
+
+    if (normal.dot(ray.direction_) > 0.0){
+        normal *= -1.0;
+    }
+
+    axis = std::abs(normal[0]) > 0.1 ? Vec3f(0.0, 1.0, 0.0) : Vec3f(1.0, 0.0, 0.0);
+
+    u = axis.cross(normal).normalize();
+    v = normal.cross(u).normalize(); // wasn't normalized before
+
+    newdir = u*cos(rand1)*rand2s + v*sin(rand1)*rand2s + normal*sqrt(1.0-rand2);
+    newdir = newdir.normalize();
+
+    ray.origin_ += ray.direction_ * ray.dist_ + normal * EPSILON;//*ray.dist_; // Made EPSILON relative, check // well guess what wasn,t a goood idea
+    ray.direction_ = newdir;
+    
     ray.colour_ += emission_;
     ray.mask_ = Vec3f(1.0);
 }
--- materials/BounceMaterial_t.h
+++ materials/BounceMaterial_t.h
@@ -3,6 +3,7 @@
 
 #include "Material_t.h"
 #include "Vec3f.h"
+#include <random>
 
 class Shape_t;
 
@@ -11,7 +12,8 @@ class BounceMaterial_t final : public Material_t{
         BounceMaterial_t(unsigned int max_bounces);
         virtual ~BounceMaterial_t() final;
 
-        Vec3f emission_;
+        Vec3f emission_;        
+        std::uniform_real_distribution<double> unif_;
 
         virtual void bounce(const double (&uv)[2], const Shape_t* hit_obj, Ray_t &ray) final;
 };
