41383bc3c0250c4ce21ad270e3a3875b9dcf3b43
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-08-20 16:36:29 -0400

    fixed array version

--- acceleration/GridCellArray_t.cpp
+++ acceleration/GridCellArray_t.cpp
@@ -1,39 +1,28 @@
-#include "GridCell_t.h"
+#include "GridCellArray_t.h"
 #include "Shape_t.h"
-#include <list>
 #include <limits>
 
-#define CELLLIST
-
-#ifdef CELLLIST
-GridCell_t::GridCell_t() : items_(std::list<Shape_t*>()) {
-#else
-GridCell_t::GridCell_t() : items_(nullptr/*std::list<Shape_t*>()*/) {
-#endif
+GridCellArray_t::GridCellArray_t() : size_(0), items_(nullptr) {
     n_obj_ = 0;
 }
 
-GridCell_t::~GridCell_t(){
-    #ifndef CELLLIST
+GridCellArray_t::GridCellArray_t(unsigned int size) : size_(size), items_(new Shape_t*[size_]) {
+    n_obj_ = 0;
+}
+
+GridCellArray_t::~GridCellArray_t(){
     if (items_ != nullptr){
         delete [] items_;
     }
-    #endif
 }
 
-void GridCell_t::update(){
-    #ifdef CELLLIST
-    for (std::list<Shape_t*>::iterator it = items_.begin(); it != items_.end(); ++it){
-        (*it)->update();
-    }
-    #else
+void GridCellArray_t::update(){
     for (unsigned int i = 0; i < n_obj_; i++){
         items_[i]->update();
     }
-    #endif
 }
 
-void GridCell_t::intersect(const Ray_t &ray, Shape_t* &hit_obj, double &t, double (&uv)[2]) const {
+void GridCellArray_t::intersect(const Ray_t &ray, Shape_t* &hit_obj, double &t, double (&uv)[2]) const {
     double t_temp;
     bool intersected;
     double uv_temp[2];
@@ -41,17 +30,6 @@ void GridCell_t::intersect(const Ray_t &ray, Shape_t* &hit_obj, double &t, doubl
     hit_obj = nullptr; // dunno if this is needed    
     t = std::numeric_limits<double>::infinity();
 
-    #ifdef CELLLIST
-    for (auto it = items_.begin(); it != items_.end(); ++it){
-        (*it)->intersection(ray, intersected, t_temp, uv_temp);
-        if (intersected && (t_temp < t)){
-            hit_obj = *it;
-            uv[0] = uv_temp[0];
-            uv[1] = uv_temp[1];
-            t = t_temp;
-        }
-    }
-    #else
     for (unsigned int i = 0; i < n_obj_; i++){
         items_[i]->intersection(ray, intersected, t_temp, uv_temp);
         if (intersected && (t_temp < t)){
@@ -61,56 +39,36 @@ void GridCell_t::intersect(const Ray_t &ray, Shape_t* &hit_obj, double &t, doubl
             t = t_temp;
         }
     }
-    #endif
 }
 
-void GridCell_t::add(Shape_t* item){
-    #ifndef CELLLIST
-    Shape_t** items2 = new Shape_t*[n_obj_+1];
-    for (unsigned int i = 0; i < n_obj_; i++){
-        items2[i] = items_[i];
+void GridCellArray_t::add(Shape_t* item){
+    if (n_obj_ < size_){
+        items_[n_obj_] = item;
     }
-    items2[n_obj_] = item;
-    if (items_ != nullptr){
-        delete [] items_;
-    }
-    items_ = items2;
-    #else
-    items_.push_back(item);
-    #endif
-
-    n_obj_++;
-}
-
-void GridCell_t::remove(const Shape_t* item){
-    #ifdef CELLLIST
-    for (std::list<Shape_t*>::iterator it = items_.begin(); it != items_.end(); ++it){
-        if ((*it) == item){
-            items_.erase(it);
-            n_obj_--;
-            break;
+    else {
+        Shape_t** items2 = new Shape_t*[n_obj_+1];
+        for (unsigned int i = 0; i < n_obj_; i++){
+            items2[i] = items_[i];
         }
-    }
-    #else
+        items2[n_obj_] = item;
+        if (items_ != nullptr){
+            delete [] items_;
+        }
+        items_ = items2;
+        ++size_;
+    }    
+
+    ++n_obj_;
+}
+
+void GridCellArray_t::remove(const Shape_t* item){
     for (unsigned int i = 0; i < n_obj_; i++){
         if (items_[i] == item){
-            
-
-            Shape_t** items2 = new Shape_t*[n_obj_-1];
-            for (unsigned int j = 0; j < i-1; j++){
-                items2[j] = items_[j];
+            for (unsigned int j = i+1; j < n_obj_; ++j){
+                items_[j-1] = items_[j]; 
             }
-            for (unsigned int j = i+1; j < n_obj_; j++){
-                items2[j-1] = items_[j];
-            }
-            if (items_ != nullptr){
-                delete [] items_;
-            }
-            items_ = items2;
-
-            n_obj_--;
+            --n_obj_;
             break;
         }
     }  
-    #endif  
 }
--- acceleration/GridCellArray_t.h
+++ acceleration/GridCellArray_t.h
@@ -1,24 +1,19 @@
-#ifndef GRIDCELL_T_H
-#define GRIDCELL_T_H
+#ifndef GRIDCELLARRAY_T_H
+#define GRIDCELLARRAY_T_H
 
 #include "Ray_t.h"
 #include "AccelerationStructure_t.h"
-#include <list>
-
-#define CELLLIST
 
 class Shape_t;
 
-class GridCell_t final : public AccelerationStructure_t{
+class GridCellArray_t final : public AccelerationStructure_t{
     public:
-        GridCell_t();
-        virtual ~GridCell_t() final;
+        GridCellArray_t();
+        GridCellArray_t(unsigned int size);
+        virtual ~GridCellArray_t() final;
 
-        #ifdef CELLLIST
-        std::list<Shape_t*> items_; // Should be list or vector or array?
-        #else
+        unsigned int size_;
         Shape_t** items_;
-        #endif
 
         virtual void update() final;
         virtual void intersect(const Ray_t &ray, Shape_t* &hit_obj, double &t, double (&uv)[2]) const final;
