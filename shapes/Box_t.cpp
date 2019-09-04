8493087b4fd3f27642d22fccf183444a61a31b80
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-09-03 20:53:21 -0400

    made box const

--- shapes/Box_t.cpp
+++ shapes/Box_t.cpp
@@ -9,21 +9,15 @@ Box_t::Box_t(Vec3f (&coord)[2]) : coordinates_{coord[0], coord[1]} {}
 Box_t::~Box_t(){}
 
 void Box_t::intersection(const Ray_t &ray, bool &intersected, double &t) const {
-    Vec3f invdir;
-    bool sign[3];
-    double tmin, tmax;
-    double tymin, tymax;
-    double tzmin, tzmax;
+    const Vec3f invdir = Vec3f(1.0)/ray.direction_;
+    const bool sign[3] = {invdir[0] < 0,
+                            invdir[1] < 0,
+                            invdir[2] < 0};
 
-    invdir = Vec3f(1.0)/ray.direction_;
-    sign[0] = invdir[0] < 0;
-    sign[1] = invdir[1] < 0;
-    sign[2] = invdir[2] < 0;
-
-    tmin = (coordinates_[sign[0]][0] - ray.origin_[0]) * invdir[0];
-    tmax = (coordinates_[!sign[0]][0] - ray.origin_[0]) * invdir[0];
-    tymin = (coordinates_[sign[1]][1] - ray.origin_[1]) * invdir[1];
-    tymax = (coordinates_[!sign[1]][1] - ray.origin_[1]) * invdir[1];
+    double tmin = (coordinates_[sign[0]][0] - ray.origin_[0]) * invdir[0];
+    double tmax = (coordinates_[!sign[0]][0] - ray.origin_[0]) * invdir[0];
+    const double tymin = (coordinates_[sign[1]][1] - ray.origin_[1]) * invdir[1];
+    const double tymax = (coordinates_[!sign[1]][1] - ray.origin_[1]) * invdir[1];
 
     if ((tmin > tymax) || (tymin > tmax)){
         intersected = false;
@@ -34,8 +28,8 @@ void Box_t::intersection(const Ray_t &ray, bool &intersected, double &t) const {
     tmin = std::max(tmin, tymin);
     tmax = std::min(tmax, tymax);
 
-    tzmin = (coordinates_[sign[2]][2] - ray.origin_[2]) * invdir[2];
-    tzmax = (coordinates_[!sign[2]][2] - ray.origin_[2]) * invdir[2];
+    const double tzmin = (coordinates_[sign[2]][2] - ray.origin_[2]) * invdir[2];
+    const double tzmax = (coordinates_[!sign[2]][2] - ray.origin_[2]) * invdir[2];
 
     if ((tmin > tzmax) || (tzmin > tmax)){
         intersected = false;
