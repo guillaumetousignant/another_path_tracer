d431f89a2c393106cd3dd92b1f29c5f772e3a5b9
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-09-07 08:06:48 -0400

    fixed segfault, was int underflow

--- entities/Texture_t.cpp
+++ entities/Texture_t.cpp
@@ -10,14 +10,14 @@ Texture_t::Texture_t(const std::string &filename){
 
     img_ = new Vec3f*[size_y_];
 
-    for (unsigned int j = 0; j < size_y_; j++){
+    for (unsigned int j = 0; j < size_y_; ++j){
         img_[j] = new Vec3f[size_x_];
     }
 
     const unsigned int n = size_x_ * size_y_;
 
-    for (unsigned int j = 0; j < size_y_; j++){
-        for (unsigned int i = 0; i < size_x_; i++){
+    for (unsigned int j = 0; j < size_y_; ++j){
+        for (unsigned int i = 0; i < size_x_; ++i){
             //img_[j][i] = Vec3f(image(i, j, 0), image(i, j, 1), image(i, j, 2));
             img_[size_y_ - j - 1][i] = Vec3f(image(i, j, 0, 0, n, n)/255.0, image(i, j, 0, 1, n, n)/255.0, image(i, j, 0, 2, n, n)/255.0);
         }
@@ -26,7 +26,7 @@ Texture_t::Texture_t(const std::string &filename){
 
 Texture_t::~Texture_t(){
     if (img_ != nullptr){
-        for (unsigned int j = 0; j < size_y_; j++){
+        for (unsigned int j = 0; j < size_y_; ++j){
             if (img_[j] != nullptr){
                 delete [] img_[j];
             }
@@ -36,16 +36,16 @@ Texture_t::~Texture_t(){
 }
 
 Vec3f Texture_t::get(const double (&xy)[2]) const {
-    const double x = ((double)size_x_ - 1.0) * xy[0];
-    const double y = ((double)size_y_ - 1.0) * xy[1];
+    const double x = (size_x_ - 1) * xy[0];
+    const double y = (size_y_ - 1) * xy[1];
 
     const double xd = x - std::floor(x);
     const double yd = y - std::floor(y);
 
-    const unsigned int xlo = (unsigned int)std::floor(x);
-    const unsigned int xhi = (unsigned int)std::ceil(x);
-    const unsigned int ylo = (unsigned int)std::floor(y);
-    const unsigned int yhi = (unsigned int)std::ceil(y);
+    const int xlo = x;      // floor
+    const int xhi = x + 1.0;// ceil
+    const int ylo = x;      // floor
+    const int yhi = x + 1.0;// ceil
 
 
     return  img_[ylo][xlo] * (1.0 - xd) * (1.0 - yd) +
@@ -55,5 +55,5 @@ Vec3f Texture_t::get(const double (&xy)[2]) const {
 }
 
 Vec3f Texture_t::get_nn(const double (&xy)[2]) const {
-    return img_[(unsigned int)std::round(((double)size_x_ - 1.0) * xy[0])][(unsigned int)std::round(((double)size_y_ - 1.0) * xy[1])];
+    return img_[std::lround((size_x_ - 1) * xy[0])][std::lround((size_y_ - 1) * xy[1])];
 }
