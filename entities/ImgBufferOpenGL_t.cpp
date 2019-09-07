6befc963ff1b9619aa3e6183f8bf17e0d174cd2c
Guillaume Tousignant <moi_guillaume@hotmail.com>
2019-09-07 09:09:21 -0400

    fixed set function

--- entities/ImgBufferOpenGL_t.cpp
+++ entities/ImgBufferOpenGL_t.cpp
@@ -4,7 +4,7 @@
 
 ImgBufferOpenGL_t::ImgBufferOpenGL_t(unsigned int size_x, unsigned int size_y): ImgBuffer_t(size_x, size_y) {
     img_gl_ = new unsigned char[size_x_*size_y_*3];
-    for (unsigned int i = 0; i < size_x_*size_y_*3; i++){
+    for (unsigned int i = 0; i < size_x_*size_y_*3; ++i){
         img_gl_[i] = 0;
     }
     tex_ = 0;
@@ -18,8 +18,8 @@ ImgBufferOpenGL_t::~ImgBufferOpenGL_t(){
 
 void ImgBufferOpenGL_t::reset(){
     updates_ = 0;
-    for (unsigned int j = 0; j < size_y_; j++){
-        for (unsigned int i = 0; i < size_x_; i++){
+    for (unsigned int j = 0; j < size_y_; ++j){
+        for (unsigned int i = 0; i < size_x_; ++i){
             img_[j][i] = Vec3f();
         }
     }
@@ -28,8 +28,8 @@ void ImgBufferOpenGL_t::reset(){
 void ImgBufferOpenGL_t::update(const Vec3f** img, unsigned int size_x, unsigned int size_y){
     ++updates_;
 
-    for (unsigned int j = 0; j < size_y; j++){
-        for (unsigned int i = 0; i < size_x; i++){
+    for (unsigned int j = 0; j < size_y; ++j){
+        for (unsigned int i = 0; i < size_x; ++i){
             img_[j][i] += img[j][i];
         }
     }
@@ -83,8 +83,8 @@ void ImgBufferOpenGL_t::update(const Vec3f &colour, unsigned int pos_x, unsigned
 
 void ImgBufferOpenGL_t::set(const Vec3f** img, unsigned int size_x, unsigned int size_y){
     updates_ = 1;
-    for (unsigned int j = 0; j < size_y; j++){
-        for (unsigned int i = 0; i < size_x; i++){
+    for (unsigned int j = 0; j < size_y; ++j){
+        for (unsigned int i = 0; i < size_x; ++i){
             img_[j][i] = img[j][i];
         }
     }
@@ -92,7 +92,7 @@ void ImgBufferOpenGL_t::set(const Vec3f** img, unsigned int size_x, unsigned int
 
 void ImgBufferOpenGL_t::set(const Vec3f &colour, unsigned int pos_x, unsigned int pos_y){
     img_[pos_y][pos_x] = colour;
-    img_gl_[((size_y_-1-pos_y)*size_x_ + pos_x)*3] = (unsigned char)(std::min(1.0, img_[pos_y][pos_x][0]/updates_) * 255.0 );
-    img_gl_[((size_y_-1-pos_y)*size_x_ + pos_x)*3+1] = (unsigned char)(std::min(1.0, img_[pos_y][pos_x][1]/updates_) * 255.0 );
-    img_gl_[((size_y_-1-pos_y)*size_x_ + pos_x)*3+2] = (unsigned char)(std::min(1.0, img_[pos_y][pos_x][2]/updates_) * 255.0 );
+    img_gl_[((size_y_-1-pos_y)*size_x_ + pos_x)*3] = std::lround(std::max(std::min(1.0, img_[pos_y][pos_x][0]/updates_), 0.0) * 255.0 );
+    img_gl_[((size_y_-1-pos_y)*size_x_ + pos_x)*3+1] = std::lround(std::max(std::min(1.0, img_[pos_y][pos_x][1]/updates_), 0.0) * 255.0 );
+    img_gl_[((size_y_-1-pos_y)*size_x_ + pos_x)*3+2] = std::lround(std::max(std::min(1.0, img_[pos_y][pos_x][2]/updates_), 0.0) * 255.0 );
 }
