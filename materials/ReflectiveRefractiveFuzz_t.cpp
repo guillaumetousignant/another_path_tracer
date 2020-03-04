d938e60d3274b57f4eb0e52270640fd2d94e3c91
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-03-04 13:07:24 -0500

    fixed weird shape

--- materials/ReflectiveRefractiveFuzz_t.cpp
+++ materials/ReflectiveRefractiveFuzz_t.cpp
@@ -20,36 +20,35 @@ void ReflectiveRefractiveFuzz_t::bounce(const double (&uv)[2], const Shape_t* hi
     double cosi = ray.direction_.dot(normal);
 
     if (priority_ >= ray.medium_list_.front()->priority_){ // CHECK also discard if priority is equal, but watch for going out case
-        Vec3f n;
         double etai, etat;
         double kr;
+
+        const double rand1 = unif_(my_rand::rng)*2.0*PI;
+        const double rand2 = std::pow(unif_(my_rand::rng), order_) * diffusivity_;
+        const double rand2s = std::sqrt(rand2);
+
+        const Vec3f axis = std::abs(normal[0]) > 0.1 ? Vec3f(0.0, 1.0, 0.0) : Vec3f(1.0, 0.0, 0.0);
+
+        const Vec3f u = axis.cross(normal).normalize_inplace();
+        const Vec3f v = normal.cross(u).normalize_inplace(); // wasn't normalized before
+
+        Vec3f normal_fuzz = (u * std::cos(rand1)*rand2s + v*std::sin(rand1)*rand2s + normal*std::sqrt(1.0-rand2)).normalize_inplace();
+
+        cosi = ray.direction_.dot(normal_fuzz);
         
         if (cosi < 0.0){ // Coming in
             etai = ray.medium_list_.front()->ind_;
             etat = ind_;
-            //cosi *= -1;
-            n = normal;
+            cosi *= -1.0;
             //coming_out = false;
         }
         else{ // Goind out
             etat = (*std::next(ray.medium_list_.begin()))->ind_;
             etai = ind_;
-            n = -normal;
+            normal_fuzz *= -1.0;
             //coming_out = true;
         }
 
-        const double rand1 = unif_(my_rand::rng)*2.0*PI;
-        const double rand2 = std::pow(unif_(my_rand::rng), order_) * diffusivity_;
-        const double rand2s = sqrt(rand2);
-
-        const Vec3f axis = std::abs(n[0]) > 0.1 ? Vec3f(0.0, 1.0, 0.0) : Vec3f(1.0, 0.0, 0.0);
-
-        const Vec3f u = axis.cross(normal).normalize_inplace();
-        const Vec3f v = normal.cross(u).normalize_inplace(); // wasn't normalized before
-
-        const Vec3f normal_fuzz = (u * std::cos(rand1)*rand2s + v*std::sin(rand1)*rand2s + normal*std::sqrt(1.0-rand2)).normalize_inplace();
-
-        cosi = ray.direction_.dot(normal_fuzz);
         const double eta = etai/etat;
         const double sint = eta * std::sqrt(1.0 - cosi * cosi);
 
@@ -83,7 +82,7 @@ void ReflectiveRefractiveFuzz_t::bounce(const double (&uv)[2], const Shape_t* hi
         }
         else{ // Reflected
             newdir = ray.direction_ - normal_fuzz * 2.0 * cosi;
-            ray.origin_ += ray.direction_ * ray.dist_ + n * EPSILON; // n or normal_fuzz?          
+            ray.origin_ += ray.direction_ * ray.dist_ + normal_fuzz * EPSILON; // n or normal_fuzz?          
         }
 
         ray.colour_ += ray.mask_ * emission_;
