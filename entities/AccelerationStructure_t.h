25f959d1dca81ebdb44a791ed2dc5ce5cbbfe81e
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-03-11 18:27:12 -0400

    Documented acceleration structure interface class

--- entities/AccelerationStructure_t.h
+++ entities/AccelerationStructure_t.h
@@ -5,15 +5,62 @@
 
 class Shape_t;
 
+/**
+ * @brief The acceleration structure class holds shapes and can be intersected.
+ * 
+ * This class is what is queried to find what shape a ray intersects. It is an interface
+ * class, defining what a concrete acceleration structure must implement. An acceleration
+ * structure should accelerate ray intersection compared to a brute-force approach.
+ */
 class AccelerationStructure_t{
     public:
+        /**
+         * @brief Construct a new AccelerationStructure_t object. Does nothing.
+         */
         AccelerationStructure_t(){};
+
+        /**
+         * @brief Destroy the AccelerationStructure_t object. Does nothing.
+         */
         virtual ~AccelerationStructure_t(){};
 
-        unsigned int n_obj_;
+        unsigned int n_obj_; /**< @brief Number of shapes currently held in the acceleration structure.*/
 
+        /**
+         * @brief Updates all the shapes in the acceleration structure.
+         * 
+         * This is a virtual function. All acceleration strucures should implement it, as it
+         * called to update all the shapes in the structure if their transformation matrix
+         * has changed.
+         */
         virtual void update() = 0;
+
+        /**
+         * @brief Intersects a ray with the acceleration structure to find the closest hit
+         * and its caracteristics.
+         * 
+         * Main function of the acceleration structure. Virtual function, it should return 
+         * the information of the closest intersection, or nullptr if no intersection. t 
+         * and uv may have undefined values if no intersection is found.
+         * 
+         * @param[in] ray Ray to be intersected with the acceleration structure, using its current origin and direction.
+         * @param[out] t Distance to intersection. It is stored in t if there is an intersection.
+         * @param[out] uv 2D object-space coordinates of the intersection.
+         * @return Shape_t* Pointer of the intersected shape. Returns nullptr if there is no intersection.
+         */
         virtual Shape_t* intersect(const Ray_t &ray, double &t, double (&uv)[2]) const = 0;
+        
+        /**
+         * @brief Adds a shape to the shape to the acceleration structure.
+         * 
+         * Virtual function to add a shape to the acceleration structure. Must be implemented to make multi-level
+         * acceleration structures possible. Not all acceleration structures are capable of
+         * inserting shapes without a refit, so this operation may be pricey and should be 
+         * considered when choosing a nested acceleration structure if it can't be constructed
+         * all at once.
+         * 
+         * @param[in] item Shape to be added to the acceleration structure.
+         */
         virtual void add(Shape_t* item) = 0;
 };
 
