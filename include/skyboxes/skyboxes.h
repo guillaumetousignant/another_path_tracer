f44b266821b5a890be3eb3bb8dab312f72bb5349
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-03-27 16:03:02 -0400

    shouldn't be a prefix

--- include/skyboxes/skyboxes.h
+++ include/skyboxes/skyboxes.h
@@ -1,7 +1,7 @@
 #ifndef SKYBOXES_H
 #define SKYBOXES_H
 
-#include "entities/Skybox_t.h"
+#include "Skybox_t.h"
 #include "SkyboxFlat_t.h"
 #include "SkyboxFlatSun_t.h"
 #include "SkyboxTexture_t.h"
