08b2575d7a5457bc851d9f36e17c4186ed86d79e
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-05-04 11:45:22 -0400

    documented more entities

--- include/entities/MaterialMap_t.h
+++ include/entities/MaterialMap_t.h
@@ -7,14 +7,46 @@
 namespace APTracer { namespace Entities {
     class Material_t;
 
+    /**
+     * @brief The material map class holds materials and their names, to find referenced materials in, forr example, obj files.
+     * 
+     * This is the class used to initialise mesh geometries with multiple materials.
+     */
     class MaterialMap_t{
         public:
+            /**
+             * @brief Construct a new MaterialMap_t object
+             * 
+             * @param names Array of strings representing the names of the materials, in order. Size: n.
+             * @param materials Array of pointers to the materials represented by the names, in order. Size: n.
+             * @param n Size of both input arrays.
+             */
             MaterialMap_t(std::string* names, Material_t** materials, unsigned int n);
+
+            /**
+             * @brief Destroy the MaterialMap_t object. Does nothing.
+             */
             ~MaterialMap_t();
 
-            std::map<std::string, Material_t*> materials_;
+            std::map<std::string, Material_t*> materials_; /**< @brief Map of material pointers using string as keys. The keys represent the materials' names.*/
 
+            /**
+             * @brief Get the material pointer associated with a specific name.
+             * 
+             * No checks are made, will throw out_of_range if the key is not present.
+             * 
+             * @param key String representing the material's name.
+             * @return Material_t* Pointer to the material which has the input name.
+             */
             Material_t* getMaterial(const std::string &key) const;
+
+            /**
+             * @brief Get the first material pointer from the map.
+             * 
+             * Useful when an object must have one material only, even if it uses multiple materials internally. Was the case for old meshes, which were shapes.
+             * 
+             * @return Material_t* Pointer to the first material of the map.
+             */
             Material_t* getFirst() const;
     };
 }}
--- include/entities/MaterialMix_t.h
+++ include/entities/MaterialMix_t.h
@@ -4,14 +4,30 @@
 #include "entities/Material_t.h"
 
 namespace APTracer { namespace Entities {
+
+    /**
+     * @brief The material mix class describes a wrapper for two materials that will intersect one or the other, depending on circumstances.
+     * 
+     * This interface class is used to bind two materials together, leaving up to the derived classes how to choose which material is used on bounce.
+     */
     class MaterialMix_t : public Material_t{
         public:
+            /**
+             * @brief Construct a new MaterialMix_t object from two materials.
+             * 
+             * @param material_refracted First material of the mix.
+             * @param material_reflected Second material of the mix.
+             */
             MaterialMix_t(Material_t* material_refracted, Material_t* material_reflected) 
                 : material_refracted_(material_refracted), material_reflected_(material_reflected) {};
+
+            /**
+             * @brief Destroy the MaterialMix_t object. Does nothing.
+             */
             virtual ~MaterialMix_t(){};
 
-            Material_t* material_refracted_;
-            Material_t* material_reflected_;        
+            Material_t* material_refracted_; /**< @brief First material of the mix. Doesn't have to be specifically refracted, though it often is.*/
+            Material_t* material_reflected_; /**< @brief Second material of the mix. Doesn't have to be specifically reflected, though it often is.*/     
     };
 }}
 
