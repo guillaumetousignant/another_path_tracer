dedd83d51b19711d5639f8555d7dad93983fcd94
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-05-06 15:02:10 -0400

    finished documenting OpenGLRenderer_t

--- include/entities/OpenGLRenderer_t.h
+++ include/entities/OpenGLRenderer_t.h
@@ -15,7 +15,7 @@ namespace APTracer { namespace Entities {
      * This class interacts with OpenGL. It binds an ImgBufferOpenGL_t to an OpenGL texture and sets up OpenGL to display it.
      * It also implements some input functions, like mouse movement and keyboard clicks. The camera's raytrace function is called 
      * each frame, and the camera is reset if it is moved. It uses a singleton pattern and all functions are duplicated to
-     * work with the OpenGL C-style API.
+     * work with the OpenGL C-style API, which uses function pointers for callbacks.
      */
     class OpenGLRenderer_t{
         public:
@@ -58,31 +58,168 @@ namespace APTracer { namespace Entities {
             bool right_clicked_; /**< @brief True if the right mouse button is currently pressed.*/
             bool left_clicked_; /**< @brief True if the left mouse button is currently pressed.*/
             bool middle_clicked_; /**< @brief True if the middle mouse button is currently pressed.*/
-            Vec3f focus_point_;
-            double camera_dist_;
-            bool updated_;
-            unsigned int write_interval_;
-            void (*render_function_)();
+            Vec3f focus_point_; /**< @brief Point around which the camera rotates.*/
+            double camera_dist_; /**< @brief Distance from the focus point to the camera.*/
+            bool updated_; /**< @brief True if the camera moved or has been changed and scene must be redrawn.*/
+            unsigned int write_interval_; /**< @brief Writes to disk every write_interval_ samples.*/
+            void (*render_function_)(); /**< @brief Function pointer to the function used at each frame for rendering the scene.*/
 
+            /**
+             * @brief Function used every frame when the rendering is on pause. Does nothing.
+             */
             void dummyDisp(){};
+
+            /**
+             * @brief Function used every frame to accumulate one sample per pixel for the whole image.
+             * 
+             * Uses the renderer's camera, scene and image buffer. 
+             */
             void accumulate();
+
+            /**
+             * @brief Function used every frame to accumulate one sample per pixel for the whole image, and save the image every write_interval_ samples.
+             * 
+             * Uses the renderer's camera, scene and image buffer. 
+             */
             void accumulate_write();
+
+            /**
+             * @brief Resets the camera and its image buffer, and starts accumulating samples from scratch.
+             */
             void resetDisplay(void);
+
+            /**
+             * @brief Function called by OpenGL on mouse movement. Moves the camera.
+             * 
+             * Makes the camera move according to mouse input if a button is clicked. Left button is zoom, middle button is pan, and right button is rotate.
+             * 
+             * @param x Horizontal position of the cursor in pixels along the window from the left side.
+             * @param y Vertical position of the cursor in pixels along the window from the bottom.
+             */
             void mouseMovement(int x, int y);
+
+            /**
+             * @brief Function called by OpenGL on mouse clicks. Saves this state for camera move.
+             * 
+             * @param button Which button of the mouse was pressed or depressed.
+             * @param state Is the button pressed or depressed.
+             * @param x Horizontal position of the cursor in pixels along the window from the left side.
+             * @param y Vertical position of the cursor in pixels along the window from the bottom.
+             */
             void mouseClick(int button, int state, int x, int y);
+
+            /**
+             * @brief Function called by OpenGL on keyboard presses. Used when the rendering is paused.
+             * 
+             * Responsible for resuming the rendering, writing to disk and quitting.
+             * 
+             * @param key Which key was pressed.
+             * @param x Horizontal position of the cursor in pixels along the window from the left side.
+             * @param y Vertical position of the cursor in pixels along the window from the bottom.
+             */
             void keyboardPaused(unsigned char key, int x, int y);
+
+            /**
+             * @brief Function called by OpenGL on keyboard presses. Used when the rendering is running.
+             * 
+             * Responsible for pausing the rendering, writing to disk, quitting and focusing.
+             * 
+             * @param key Which key was pressed.
+             * @param x Horizontal position of the cursor in pixels along the window from the left side.
+             * @param y Vertical position of the cursor in pixels along the window from the bottom.
+             */
             void keyboard(unsigned char key, int x, int y);
+
+            /**
+             * @brief Initialises the OpenGL environment and binds the render output to it.
+             */
             void initialise();
+
+            /**
+             * @brief Launches the rendering, calling glutMainLoop.
+             */
             void render();
+
+            /**
+             * @brief Launches the rendering, calling glutMainLoop, and save the image every write_interval_ samples.
+             */
             void render_write(unsigned int write_interval = 1);
         
+            /**
+             * @brief Wrapper function used every frame when the rendering is on pause. Does nothing.
+             * 
+             * This calls the dummyDisp function on the singleton OpenGLRenderer_t object.
+             */
             static void openGL_dummyDisp();
+
+            /**
+             * @brief Wrapper function used every frame to accumulate one sample per pixel for the whole image.
+             * 
+             * Uses the renderer's camera, scene and image buffer. 
+             * This calls the accumulate function on the singleton OpenGLRenderer_t object.
+             */
             static void openGL_accumulate();
+
+            /**
+             * @brief Wrapper function used every frame to accumulate one sample per pixel for the whole image, and save the image every write_interval_ samples.
+             * 
+             * Uses the renderer's camera, scene and image buffer. 
+             * This calls the accumulate_write function on the singleton OpenGLRenderer_t object.
+             */
             static void openGL_accumulate_write();
+
+            /**
+             * @brief Wrapper function that resets the camera and its image buffer, and starts accumulating samples from scratch.
+             * 
+             * This calls the accumulate_write function on the singleton OpenGLRenderer_t object.
+             */
             static void openGL_resetDisplay(void);
+
+            /**
+             * @brief Wrapper function called by OpenGL on mouse movement. Moves the camera.
+             * 
+             * Makes the camera move according to mouse input if a button is clicked. Left button is zoom, middle button is pan, and right button is rotate.
+             * This calls the mouseMovement function on the singleton OpenGLRenderer_t object.
+             * 
+             * @param x Horizontal position of the cursor in pixels along the window from the left side.
+             * @param y Vertical position of the cursor in pixels along the window from the bottom.
+             */
             static void openGL_mouseMovement(int x, int y);
+
+            /**
+             * @brief Wrapper function called by OpenGL on mouse clicks. Saves this state for camera move.
+             * 
+             * This calls the mouseClick function on the singleton OpenGLRenderer_t object.
+             * 
+             * @param button Which button of the mouse was pressed or depressed.
+             * @param state Is the button pressed or depressed.
+             * @param x Horizontal position of the cursor in pixels along the window from the left side.
+             * @param y Vertical position of the cursor in pixels along the window from the bottom.
+             */
             static void openGL_mouseClick(int button, int state, int x, int y);
+
+            /**
+             * @brief Wrapper function called by OpenGL on keyboard presses. Used when the rendering is paused.
+             * 
+             * Responsible for resuming the rendering, writing to disk and quitting.
+             * This calls the keyboardPaused function on the singleton OpenGLRenderer_t object.
+             * 
+             * @param key Which key was pressed.
+             * @param x Horizontal position of the cursor in pixels along the window from the left side.
+             * @param y Vertical position of the cursor in pixels along the window from the bottom.
+             */
             static void openGL_keyboardPaused(unsigned char key, int x, int y);
+
+            /**
+             * @brief Wrapper function called by OpenGL on keyboard presses. Used when the rendering is running.
+             * 
+             * Responsible for pausing the rendering, writing to disk, quitting and focusing.
+             * This calls the keyboardPaused function on the singleton OpenGLRenderer_t object.
+             * 
+             * @param key Which key was pressed.
+             * @param x Horizontal position of the cursor in pixels along the window from the left side.
+             * @param y Vertical position of the cursor in pixels along the window from the bottom.
+             */
             static void openGL_keyboard(unsigned char key, int x, int y);
     };
 }}
