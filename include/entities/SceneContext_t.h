940c762c059593f6b07785937c5e1d6b41eec3ab
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-05-11 19:48:10 -0400

    started documenting scene context (it's so biiiig)

--- include/entities/SceneContext_t.h
+++ include/entities/SceneContext_t.h
@@ -33,69 +33,244 @@ using namespace APTracer::Shapes;
 using namespace APTracer::Entities;
 
 namespace APTracer { namespace Entities {
+
+    /**
+     * @brief The scene context class contains all data required to draw a static scene. It can read xml scene files representing many objects.
+     * 
+     * The scene context holds arrays of every object needed to render a static scene. It uses smart pointers everywhere to simplify memory management. 
+     * The scene context can be initialised by hand, or can read from xml scene files. Several of those files are included with the project, in the
+     * scenes folder. Refer to them for format.
+     */
     class SceneContext_t{
         public:
+            /**
+             * @brief Construct a new empty SceneContext_t object.
+             */
             SceneContext_t();
+
+            /**
+             * @brief Destroy the SceneContext_t object. Calls the reset function.
+             */
             ~SceneContext_t();
 
-            bool use_gl_;
-            std::string scene_name_;
-            std::unique_ptr<OpenGLRenderer_t> opengl_renderer_;
-            ImgBufferOpenGL_t* opengl_imgbuffer_; // weak ptr
-            Camera_t* opengl_camera_; // weak ptr
-            std::unique_ptr<Scene_t> scene_;
+            bool use_gl_; /**< @brief True if the scenes will be shown on the screen with OpenGL.*/
+            std::string scene_name_; /**< @brief Name of the scene. Default image filename.*/
+            std::unique_ptr<OpenGLRenderer_t> opengl_renderer_; /**< @brief OpenGL renderer object that will render the scene, if enabled.*/
+            ImgBufferOpenGL_t* opengl_imgbuffer_; /**< @brief Pointer to the imagebuffer to be used by OpenGL. Does not own the object.*/ // weak ptr
+            Camera_t* opengl_camera_; /**< @brief Pointer to the camera to be used by OpenGL. Does not own the object.*/ // weak ptr
+            std::unique_ptr<Scene_t> scene_; /**< @brief Scene to be rendered.*/
 
-            std::vector<std::string> camera_rendermode_;
-            std::vector<unsigned int> camera_n_iter_;
-            std::vector<unsigned int> camera_write_interval_;
+            std::vector<std::string> camera_rendermode_; /**< @brief Vector of strings representing the rendering mode of every camera.*/
+            std::vector<unsigned int> camera_n_iter_; /**< @brief Vector of the number of samples to accumulate per camera.*/
+            std::vector<unsigned int> camera_write_interval_; /**< @brief Vector of the write interval of cameras.*/
 
-            unsigned int index_transform_matrices_;
-            unsigned int index_textures_;
-            unsigned int index_mediums_;
-            unsigned int index_materials_;
-            unsigned int index_mesh_geometries_;
-            unsigned int index_objects_;
-            unsigned int index_directional_lights_;
-            unsigned int index_skyboxes_;
-            unsigned int index_imgbuffers_;
-            unsigned int index_cameras_;
+            unsigned int index_transform_matrices_; /**< @brief Index of the next transformation matrix in the vector.*/
+            unsigned int index_textures_; /**< @brief Index of the next texture in the vector.*/
+            unsigned int index_mediums_; /**< @brief Index of the next medium in the vector.*/
+            unsigned int index_materials_; /**< @brief Index of the next material in the vector.*/
+            unsigned int index_mesh_geometries_; /**< @brief Index of the next mesh geometry in the vector.*/
+            unsigned int index_objects_; /**< @brief Index of the next shape in the vector.*/
+            unsigned int index_directional_lights_; /**< @brief Index of the next directional light in the vector.*/
+            unsigned int index_skyboxes_; /**< @brief Index of the next skybox in the vector.*/
+            unsigned int index_imgbuffers_; /**< @brief Index of the next image buffer in the vector.*/
+            unsigned int index_cameras_; /**< @brief Index of the next camera in the vector.*/
             
-            std::vector<std::unique_ptr<TransformMatrix_t>> transform_matrices_;
-            std::vector<std::unique_ptr<Texture_t>> textures_;
-            std::vector<std::unique_ptr<Medium_t>> mediums_;
-            std::vector<std::unique_ptr<Material_t>> materials_;
-            std::vector<std::unique_ptr<MeshGeometry_t>> mesh_geometries_;
-            std::vector<std::unique_ptr<Shape_t>> objects_;
-            std::vector<std::unique_ptr<DirectionalLight_t>> directional_lights_;
-            std::vector<std::unique_ptr<Skybox_t>> skyboxes_;
-            std::vector<std::unique_ptr<ImgBuffer_t>> imgbuffers_;
-            std::vector<std::unique_ptr<Camera_t>> cameras_;
-            std::vector<std::unique_ptr<MaterialMap_t>> material_aggregates_;
-            std::vector<std::unique_ptr<MeshTop_t>> meshes_;
+            std::vector<std::unique_ptr<TransformMatrix_t>> transform_matrices_; /**< @brief Vector containing the transformation matrices.*/
+            std::vector<std::unique_ptr<Texture_t>> textures_; /**< @brief Vector containing the textures.*/
+            std::vector<std::unique_ptr<Medium_t>> mediums_; /**< @brief Vector containing the mediums.*/
+            std::vector<std::unique_ptr<Material_t>> materials_; /**< @brief Vector containing the materials.*/
+            std::vector<std::unique_ptr<MeshGeometry_t>> mesh_geometries_; /**< @brief Vector containing the mesh geometries.*/
+            std::vector<std::unique_ptr<Shape_t>> objects_; /**< @brief Vector containing the shapes.*/
+            std::vector<std::unique_ptr<DirectionalLight_t>> directional_lights_; /**< @brief Vector containing the directional lights.*/
+            std::vector<std::unique_ptr<Skybox_t>> skyboxes_; /**< @brief Vector containing the skyboxes.*/
+            std::vector<std::unique_ptr<ImgBuffer_t>> imgbuffers_; /**< @brief Vector containing the image buffers.*/
+            std::vector<std::unique_ptr<Camera_t>> cameras_; /**< @brief Vector containing the cameras.*/
+            std::vector<std::unique_ptr<MaterialMap_t>> material_aggregates_; /**< @brief Vector containing the material aggregates. Elements are nullptr for regular materials.*/
+            std::vector<std::unique_ptr<MeshTop_t>> meshes_; /**< @brief Vector containing the meshes.*/
 
+            /**
+             * @brief Reads a scene xml file, initialising the scene context from it.
+             * 
+             * Example files are available in the scenes folder.
+             * 
+             * @param filename Path to the scene file to be read, in xml format.
+             */
             void readXML(const std::string &filename);
+
+            /**
+             * @brief Starts rendering of all the scene's cameras, according to their mode.
+             */
             void render();
         
         private:
+            /**
+             * @brief Set the scene context to empty, so it can be filled again or deleted.
+             */
             void reset();
 
+            /**
+             * @brief Create a transformation matrix object from an xml entry.
+             * 
+             * @param xml_transform_matrix XML entry containing the transformation matrix information.
+             * @return std::unique_ptr<TransformMatrix_t> Unique pointer to the created transformation matrix.
+             */
             std::unique_ptr<TransformMatrix_t> create_transform_matrix(const tinyxml2::XMLElement* xml_transform_matrix) const;
+
+            /**
+             * @brief Create a texture object from an xml entry.
+             * 
+             * @param xml_texture XML entry containing the texture information.
+             * @return std::unique_ptr<Texture_t> Unique pointer to the created texture.
+             */
             std::unique_ptr<Texture_t> create_texture(const tinyxml2::XMLElement* xml_texture) const;
+
+            /**
+             * @brief Create a medium object from an xml entry.
+             * 
+             * @param xml_medium XML entry containing the medium information.
+             * @param[out] mediums_medium_list List of mediums in which the current medium is placed, if needed.
+             * @param xml_transform_matrices XML entries of all the transformation matrices.
+             * @param xml_mediums XML entries of all the mediums.
+             * @return std::unique_ptr<Medium_t> Unique pointer to the created medium.
+             */
             std::unique_ptr<Medium_t> create_medium(const tinyxml2::XMLElement* xml_medium, std::unique_ptr<std::list<unsigned int>> &mediums_medium_list, const tinyxml2::XMLElement* xml_transform_matrices, const tinyxml2::XMLElement* xml_mediums);
+            
+            /**
+             * @brief Create a material object from an xml entry.
+             * 
+             * @param xml_material XML entry containing the material information.
+             * @param[out] materials_medium_list List of mediums in which the current material is placed, if needed.
+             * @param[out] materials_mix_list Indexes of the materials making up a material mix, if needed.
+             * @param[out] materials_aggregate_list Tuple of a list of material indices and a list of material names making up a material aggregate, if needed.
+             * @param xml_textures XML entries of all the textures.
+             * @param xml_transform_matrices XML entries of all the transformation matrices.
+             * @param xml_materials XML entries of all the materials.
+             * @param xml_mediums XML entries of all the mediums.
+             * @return std::unique_ptr<Material_t> Unique pointer to the created material.
+             */
             std::unique_ptr<Material_t> create_material(const tinyxml2::XMLElement* xml_material, std::unique_ptr<std::list<unsigned int>> &materials_medium_list, std::vector<unsigned int> &materials_mix_list, std::unique_ptr<std::tuple<std::unique_ptr<std::list<unsigned int>>, std::unique_ptr<std::list<std::string>>>> &materials_aggregate_list, const tinyxml2::XMLElement* xml_textures, const tinyxml2::XMLElement* xml_transform_matrices, const tinyxml2::XMLElement* xml_materials, const tinyxml2::XMLElement* xml_mediums);
+            
+            /**
+             * @brief Create a mesh geometry object from an xml entry.
+             * 
+             * @param xml_mesh_geometry XML entry containing the mesh geometry information.
+             * @return std::unique_ptr<MeshGeometry_t> Unique pointer to the created mesh geometry.
+             */
             std::unique_ptr<MeshGeometry_t> create_mesh_geometry(const tinyxml2::XMLElement* xml_mesh_geometry) const;
+
+            /**
+             * @brief Create a shape object from an xml entry.
+             * 
+             * @param xml_object XML entry containing the shape information.
+             * @param[out] mesh Unique pointer to the created mesh, if the shape is a mesh. In this case, the return value is nullptr.
+             * @param xml_transform_matrices XML entries of all the transformation matrices.
+             * @param xml_materials XML entries of all the materials.
+             * @param xml_mesh_geometries XML entries of all the mesh geometries.
+             * @return std::unique_ptr<Shape_t> Unique pointer to the created shape. Nullptr if the shape is a mesh.
+             */
             std::unique_ptr<Shape_t> create_object(const tinyxml2::XMLElement* xml_object, std::unique_ptr<MeshTop_t> &mesh, const tinyxml2::XMLElement* xml_transform_matrices, const tinyxml2::XMLElement* xml_materials, const tinyxml2::XMLElement* xml_mesh_geometries);
+            
+            /**
+             * @brief Create a directional light object from an xml entry.
+             * 
+             * @param xml_directional_light XML entry containing the directional light information.
+             * @param xml_transform_matrices XML entries of all the transformation matrices.
+             * @return std::unique_ptr<DirectionalLight_t> Unique pointer to the created directional light.
+             */
             std::unique_ptr<DirectionalLight_t> create_directional_light(const tinyxml2::XMLElement* xml_directional_light, const tinyxml2::XMLElement* xml_transform_matrices);
+            
+            /**
+             * @brief Create a skybox object from an xml entry.
+             * 
+             * @param xml_skybox XML entry containing the skybox information.
+             * @param xml_textures XML entries of all the textures.
+             * @param xml_transform_matrices XML entries of all the transformation matrices.
+             * @param xml_directional_lights XML entries of all the directional lights.
+             * @return std::unique_ptr<Skybox_t> Unique pointer to the created skybox.
+             */
             std::unique_ptr<Skybox_t> create_skybox(const tinyxml2::XMLElement* xml_skybox, const tinyxml2::XMLElement* xml_textures, const tinyxml2::XMLElement* xml_transform_matrices, const tinyxml2::XMLElement* xml_directional_lights);
+            
+            /**
+             * @brief Create a image buffer object from an xml entry.
+             * 
+             * @param xml_imgbuffer XML entry containing the image buffer information.
+             * @return std::unique_ptr<ImgBuffer_t> Unique pointer to the created image buffer.
+             */
             std::unique_ptr<ImgBuffer_t> create_imgbuffer(const tinyxml2::XMLElement* xml_imgbuffer);
+
+            /**
+             * @brief Create a camera object from an xml entry.
+             * 
+             * @param xml_camera XML entry containing the camera information.
+             * @param next_filename Path to the next available filename comprised of the scene name and a sequential number.
+             * @param xml_transform_matrices XML entries of all the transformation matrices.
+             * @param xml_mediums XML entries of all the mediums.
+             * @param xml_imgbuffers XML entries of all the image buffers.
+             * @param xml_skyboxes XML entries of all the skyboxes.
+             * @return std::unique_ptr<Camera_t> Unique pointer to the created camera.
+             */
             std::unique_ptr<Camera_t> create_camera(const tinyxml2::XMLElement* xml_camera, const std::string &next_filename, const tinyxml2::XMLElement* xml_transform_matrices, const tinyxml2::XMLElement* xml_mediums, const tinyxml2::XMLElement* xml_imgbuffers, const tinyxml2::XMLElement* xml_skyboxes);
+            
+            /**
+             * @brief Create a acceleration structure object from an xml entry and bind it to the scene.
+             * 
+             * @param xml_acceleration_structure XML entry containing the acceleration structurecod information.
+             */
             void create_acceleration_structure(const tinyxml2::XMLElement* xml_acceleration_structure);
 
+            /**
+             * @brief Get a transform matrix object from its name or index.
+             * 
+             * @param transform_matrix String containing a transformation matrix name or index.
+             * @param xml_transform_matrices XML entries of all the transformation matrices.
+             * @return TransformMatrix_t* Pointer to the requested transformation matrix.
+             */
             TransformMatrix_t* get_transform_matrix(std::string transform_matrix, const tinyxml2::XMLElement* xml_transform_matrices);
+
+            /**
+             * @brief Get a list of material indices from a string of names of indices.
+             * 
+             * @param string_material_list String containing material names and/or indices.
+             * @param xml_materials XML entries of all the materials.
+             * @return std::unique_ptr<std::list<unsigned int>> List of material indices.
+             */
             std::unique_ptr<std::list<unsigned int>> get_material_index_list(std::string string_material_list, const tinyxml2::XMLElement* xml_materials) const;
+
+            /**
+             * @brief Get a list of medium indices from a string of names of indices.
+             * 
+             * @param string_medium_list String containing medium names and/or indices.
+             * @param xml_mediums XML entries of all the mediums.
+             * @return std::unique_ptr<std::list<unsigned int>> List of medium indices.
+             */
             std::unique_ptr<std::list<unsigned int>> get_medium_index_list(std::string string_medium_list, const tinyxml2::XMLElement* xml_mediums) const;
+
+            /**
+             * @brief Get a list of mediums from a string of names of indices.
+             * 
+             * @param string_medium_list String containing medium names and/or indices.
+             * @param xml_mediums XML entries of all the mediums.
+             * @return std::list<Medium_t*> List of mediums.
+             */
             std::list<Medium_t*> get_medium_list(std::string string_medium_list, const tinyxml2::XMLElement* xml_mediums) const;
+
+            /**
+             * @brief Get a texture from its name or index.
+             * 
+             * @param texture String containing a texture's name or index.
+             * @param xml_textures XML entries of all the textures.
+             * @return Texture_t* Pointer to the requested texture.
+             */
             Texture_t* get_texture(std::string texture, const tinyxml2::XMLElement* xml_textures) const;
+
+            /**
+             * @brief Get the material mix object
+             * 
+             * @param material_refracted 
+             * @param material_reflected 
+             * @param xml_materials 
+             * @return std::vector<unsigned int> 
+             */
             std::vector<unsigned int> get_material_mix(std::string material_refracted, std::string material_reflected, const tinyxml2::XMLElement* xml_materials) const;
             Medium_t* get_medium(std::string medium, const tinyxml2::XMLElement* xml_mediums) const;
             MeshGeometry_t* get_mesh_geometry(std::string mesh_geometry, const tinyxml2::XMLElement* xml_mesh_geometries) const;
