e2f9b364d2c4c161cfc7132c44200cc92452d326
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-05-15 13:48:35 -0400

    Documented skybox

--- include/entities/Skybox_t.h
+++ include/entities/Skybox_t.h
@@ -4,11 +4,31 @@
 #include "entities/Vec3f.h"
 
 namespace APTracer { namespace Entities {
+
+    /**
+     * @brief Skyboxes represent the infinitely far background of a scene. Define how the ray gets coloured when it doesn't intersect geometry.
+     * 
+     * Skyboxes return the colour of the background based on ray direction only. Using only the direction places the skybox infinitely far.
+     * The colour returned is used as an emissive colour, and thus lights up the scene as a colour source.
+     */
     class Skybox_t{
         public:
+            /**
+             * @brief Construct a new Skybox_t object. Does nothing.
+             */
             Skybox_t(){};
+
+            /**
+             * @brief Destroy the Skybox_t object. Does nothing.
+             */
             virtual ~Skybox_t(){};
 
+            /**
+             * @brief Returns the colour of the skybox for a given direction.
+             * 
+             * @param xyz Direction in cartesian coordinates for which we want the colour.
+             * @return Vec3f Colour of the skybox for the given direction.
+             */
             virtual Vec3f get(const Vec3f &xyz) const = 0;
     };
 }}
