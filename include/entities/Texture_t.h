d2c82b200582f53bfad103091fbc8489b0fe1f85
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-05-19 15:36:54 -0400

    documented texture

--- include/entities/Texture_t.h
+++ include/entities/Texture_t.h
@@ -5,16 +5,49 @@
 #include <string>
 
 namespace APTracer { namespace Entities {
+
+    /**
+     * @brief The texture class represents an image or other 2D buffer that can be accessed using 2D coordinates.
+     * 
+     * This is used to store image or other 2D maps, such as normal maps or emissivity maps. They can be accessed
+     * in 2D coordinates, for example using texture coordinates from meshes.
+     */
     class Texture_t{
         public:
+            /**
+             * @brief Construct a new Texture_t object from an image file.
+             * 
+             * @param filename Path to the image to be stored in the texture.
+             */
             Texture_t(const std::string &filename);
+
+            /**
+             * @brief Destroy the Texture_t object, releasing the ressources held by it.
+             */
             ~Texture_t();
 
-            unsigned int size_x_;
-            unsigned int size_y_;
-            Vec3f* img_;
+            unsigned int size_x_; /**< @brief Horizontal number of pixels in the texture*/
+            unsigned int size_y_; /**< @brief Vertical number of pixels in the texture*/
+            Vec3f* img_; /**< @brief Array of 3-element pixels representing the values of all pixels of the texture.*/
 
+            /**
+             * @brief Returns the value of the texture from 2D uniform coordinates, using bilinear filtering.
+             * 
+             * This returns the blended value of the four pixels around the given point, according to their proximity.
+             * 
+             * @param xy Array of two coordinates, [x, y], ranging from 0 to 1, starting from the bottom left of the texture.
+             * @return Vec3f Value of the texture at the provided coordinates.
+             */
             Vec3f get(const double (&xy)[2]) const;
+
+            /**
+             * @brief Returns the value of the texture from 2D uniform coordinates, using nearest-neighbour filtering.
+             * 
+             * This returns the value of the closest pixel to the given point.
+             * 
+             * @param xy Array of two coordinates, [x, y], ranging from 0 to 1, starting from the bottom left of the texture.
+             * @return Vec3f Value of the texture at the provided coordinates.
+             */
             Vec3f get_nn(const double (&xy)[2]) const;
     };
 }}
