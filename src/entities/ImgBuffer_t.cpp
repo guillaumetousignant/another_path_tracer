879bde8901322cca627ca8cd82b55e3304ca02ab
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-06-24 12:13:38 -0400

    added exr support

--- src/entities/ImgBuffer_t.cpp
+++ src/entities/ImgBuffer_t.cpp
@@ -2,6 +2,9 @@
 #define cimg_use_jpeg
 #define cimg_use_png
 #define cimg_use_tiff
+#define cimg_use_tinyexr
+#define cimg_use_openmp 1
+#define cimg_use_cpp11 1
 #define cimg_display 0
 #include "functions/CImg.h"
 #include <cmath>
