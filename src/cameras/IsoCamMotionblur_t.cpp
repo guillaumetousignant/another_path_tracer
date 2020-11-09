8b24572400ea9435739e97e59b76436d4e51623d
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-11-09 14:44:32 -0500

    MSVC openmp can now use unsigned indices

--- src/cameras/IsoCamMotionblur_t.cpp
+++ src/cameras/IsoCamMotionblur_t.cpp
@@ -44,16 +44,11 @@ void IsoCamMotionblur_t::raytrace(const Scene_t* scene) {
     const double pixel_span_x = fov_[1]/image_->size_x_;
     const double subpix_span_y = pixel_span_y/subpix_[0];
     const double subpix_span_x = pixel_span_x/subpix_[1];
-    #ifdef _WIN32
-        int index; // Openmp on windows can't use unsigned index.
-    #else
-        unsigned int index;
-    #endif
 
     image_->update();
 
     #pragma omp parallel for schedule(guided)
-    for (index = 0; index < image_->size_y_ * image_->size_x_; ++index){
+    for (unsigned int index = 0; index < image_->size_y_ * image_->size_x_; ++index){
         const unsigned int i = index%image_->size_x_;
         const unsigned int j = index/image_->size_x_;
         Vec3f col = Vec3f(); // Or declare above?
