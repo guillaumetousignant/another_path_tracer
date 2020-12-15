6c594292a44a40c4cdd6765444aced5a08fa4e63
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-12-15 09:08:42 -0500

    why using here

--- include/entities/RandomGenerator_t.h
+++ include/entities/RandomGenerator_t.h
@@ -3,9 +3,7 @@
 
 #include <random> 
 
-using std::mt19937;
-
 namespace APTracer { namespace Entities {
-    extern mt19937 rng; /**< @brief Global random generator initialised once per thread and used through the program.*/
+    extern std::mt19937 rng; /**< @brief Global random generator initialised once per thread and used through the program.*/
 }}
 #endif
