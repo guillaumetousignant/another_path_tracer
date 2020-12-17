a6f42547b99ef7d2e7f079d2c00ac5ef1c855a5d
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-12-17 13:59:34 -0500

    don't need to init with empty string

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "another_path_tracer.h"
 
 int main(int argc, char **argv){
-    std::string filename = "";
+    std::string filename;
     if (argc < 2){
         std::cout << "Enter scene path:" << std::endl;
         while (filename.empty()){
