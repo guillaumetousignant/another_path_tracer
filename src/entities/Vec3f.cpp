2bed64b4d0d4f3cfecd771345a7cac78e765e40c
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-12-27 17:45:43 -0500

    implicit return type

--- src/entities/Vec3f.cpp
+++ src/entities/Vec3f.cpp
@@ -23,31 +23,31 @@ auto Vec3f::operator[](unsigned int index) const -> double {
     return v[index];
 } 
 auto Vec3f::operator*(double scale) const -> Vec3f {
-    return Vec3f(v[0] * scale, v[1] * scale, v[2] * scale);
+    return {v[0] * scale, v[1] * scale, v[2] * scale};
 } 
 auto Vec3f::operator*(const Vec3f &other) const -> Vec3f {
-    return Vec3f(v[0] * other[0], v[1] * other[1], v[2] * other[2]);
+    return {v[0] * other[0], v[1] * other[1], v[2] * other[2]};
 } 
 auto Vec3f::operator/(double scale) const -> Vec3f {
-    return Vec3f(v[0] / scale, v[1] / scale, v[2] / scale);
+    return {v[0] / scale, v[1] / scale, v[2] / scale};
 } 
 auto Vec3f::operator/(const Vec3f &other) const -> Vec3f {
-    return Vec3f(v[0] / other[0], v[1] / other[1], v[2] / other[2]);
+    return {v[0] / other[0], v[1] / other[1], v[2] / other[2]};
 }
 auto Vec3f::operator+(const Vec3f &other) const -> Vec3f {
-    return Vec3f(v[0] + other.v[0], v[1] + other.v[1], v[2] + other.v[2]);
+    return {v[0] + other.v[0], v[1] + other.v[1], v[2] + other.v[2]};
 } 
 auto Vec3f::operator+(double factor) const -> Vec3f {
-    return Vec3f(v[0] + factor, v[1] + factor, v[2] + factor);
+    return {v[0] + factor, v[1] + factor, v[2] + factor};
 } 
 auto Vec3f::operator-(const Vec3f &other) const -> Vec3f {
-    return Vec3f(v[0] - other.v[0], v[1] - other.v[1], v[2] - other.v[2]);
+    return {v[0] - other.v[0], v[1] - other.v[1], v[2] - other.v[2]};
 } 
 auto Vec3f::operator-(double factor) const -> Vec3f {
-    return Vec3f(v[0] - factor, v[1] - factor, v[2] - factor);
+    return {v[0] - factor, v[1] - factor, v[2] - factor};
 }
 auto Vec3f::operator-() const -> Vec3f {
-    return Vec3f(-v[0], -v[1], -v[2]);
+    return {-v[0], -v[1], -v[2]};
 } 
 auto Vec3f::operator==(const Vec3f &other) const -> bool {
     return (v[0] == other.v[0]) && (v[1] == other.v[1]) && (v[2] == other.v[2]);
@@ -125,16 +125,16 @@ auto Vec3f::max(double other) -> Vec3f& {
     return *this;
 }
 auto Vec3f::getMin(const Vec3f &other) const -> Vec3f {
-    return Vec3f(std::min(v[0], other[0]), std::min(v[1], other[1]), std::min(v[2], other[2]));
+    return {std::min(v[0], other[0]), std::min(v[1], other[1]), std::min(v[2], other[2])};
 }
 auto Vec3f::getMin(double other) const -> Vec3f {
-    return Vec3f(std::min(v[0], other), std::min(v[1], other), std::min(v[2], other));
+    return {std::min(v[0], other), std::min(v[1], other), std::min(v[2], other)};
 }
 auto Vec3f::getMax(const Vec3f &other) const -> Vec3f {
-    return Vec3f(std::max(v[0], other[0]), std::max(v[1], other[1]), std::max(v[2], other[2]));
+    return {std::max(v[0], other[0]), std::max(v[1], other[1]), std::max(v[2], other[2])};
 }
 auto Vec3f::getMax(double other) const -> Vec3f {
-    return Vec3f(std::max(v[0], other), std::max(v[1], other), std::max(v[2], other));
+    return {std::max(v[0], other), std::max(v[1], other), std::max(v[2], other)};
 }
 auto Vec3f::magnitude() const -> double {
     return std::sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
@@ -144,7 +144,7 @@ auto Vec3f::magnitudeSquared() const -> double {
 } 
 auto Vec3f::normalize() const -> Vec3f {
     const double m = std::sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-    return Vec3f(v[0] / m, v[1] / m, v[2] / m);
+    return {v[0] / m, v[1] / m, v[2] / m};
 } 
 auto Vec3f::normalize_inplace() -> const Vec3f& {
     const double m = std::sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
@@ -157,9 +157,9 @@ auto Vec3f::dot(const Vec3f &other) const -> double {
     return v[0] * other.v[0] + v[1] * other.v[1] + v[2] * other.v[2];
 } 
 auto Vec3f::cross(const Vec3f &other) const -> Vec3f {
-    return Vec3f(v[1] * other.v[2] - v[2] * other.v[1],
-                 v[2] * other.v[0] - v[0] * other.v[2],
-                 v[0] * other.v[1] - v[1] * other.v[0]);
+    return {v[1] * other.v[2] - v[2] * other.v[1],
+            v[2] * other.v[0] - v[0] * other.v[2],
+            v[0] * other.v[1] - v[1] * other.v[0]};
 } 
 auto Vec3f::to_sph() -> const Vec3f& { // CHECK outputs nan
     // [r, theta, phi] (theta is polar angle)
@@ -188,25 +188,25 @@ auto Vec3f::to_xyz_offset(const Vec3f &ref1, const Vec3f &ref2, const Vec3f &ref
 auto Vec3f::get_sph() const -> Vec3f {
     const double r = magnitude();
     const double temp = std::acos(v[2]/r);
-    return Vec3f(r, std::isnan(temp) ? 0.0 : temp, std::atan2(v[1], v[0]));
+    return {r, std::isnan(temp) ? 0.0 : temp, std::atan2(v[1], v[0])};
 }
 auto Vec3f::get_xyz() const -> Vec3f {
-    return Vec3f(v[0]*std::sin(v[1])*std::cos(v[2]), v[0]*std::sin(v[1])*std::sin(v[2]), v[0]*std::cos(v[1]));
+    return {v[0]*std::sin(v[1])*std::cos(v[2]), v[0]*std::sin(v[1])*std::sin(v[2]), v[0]*std::cos(v[1])};
 }
 auto Vec3f::get_xyz_offset(const Vec3f &ref1, const Vec3f &ref2, const Vec3f &ref3) const -> Vec3f {
     return ref1 * v[0]*std::sin(v[1])*std::cos(v[2]) + ref2 * v[0]*std::sin(v[1])*std::sin(v[2]) + ref3 * v[0]*std::cos(v[1]);
 }
 auto Vec3f::ln() const -> Vec3f {
-    return Vec3f(std::log(v[0]), std::log(v[1]), std::log(v[2]));
+    return {std::log(v[0]), std::log(v[1]), std::log(v[2])};
 }
 auto Vec3f::sqrt() const -> Vec3f {
-    return Vec3f(std::sqrt(v[0]), std::sqrt(v[1]), std::sqrt(v[2]));
+    return {std::sqrt(v[0]), std::sqrt(v[1]), std::sqrt(v[2])};
 }
 auto Vec3f::exp() const -> Vec3f {
-    return Vec3f(std::exp(v[0]), std::exp(v[1]), std::exp(v[2]));
+    return {std::exp(v[0]), std::exp(v[1]), std::exp(v[2])};
 }
 auto Vec3f::pow(double exp) const -> Vec3f {
-    return Vec3f(std::pow(v[0], exp), std::pow(v[1], exp), std::pow(v[2], exp));
+    return {std::pow(v[0], exp), std::pow(v[1], exp), std::pow(v[2], exp)};
 }
 auto Vec3f::pow_inplace(double exp) -> Vec3f& {
     v[0] = std::pow(v[0], exp);
@@ -215,10 +215,10 @@ auto Vec3f::pow_inplace(double exp) -> Vec3f& {
     return *this;
 }
 auto Vec3f::floor() const -> Vec3f {
-    return Vec3f(std::floor(v[0]), std::floor(v[1]), std::floor(v[2]));
+    return {std::floor(v[0]), std::floor(v[1]), std::floor(v[2])};
 }
 auto Vec3f::ceil() const -> Vec3f {
-    return Vec3f(std::ceil(v[0]), std::ceil(v[1]), std::ceil(v[2]));
+    return {std::ceil(v[0]), std::ceil(v[1]), std::ceil(v[2])};
 }
 auto Vec3f::round_inplace() -> Vec3f& {
     v[0] = std::round(v[0]);
@@ -236,7 +236,7 @@ auto operator<<(std::ostream &output, const Vec3f &v) -> std::ostream& {
     return output;
 } 
 auto operator*(const double factor, const Vec3f &v) -> Vec3f {
-    return Vec3f(v[0] * factor, v[1] * factor, v[2] * factor);
+    return {v[0] * factor, v[1] * factor, v[2] * factor};
 }
 auto Vec3f::x() const -> double {
     return v[0];
