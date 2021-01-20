1c5047d36e40be2575be63f05a4a73bcacf5f413
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-01-20 10:38:22 -0500

    Revert "Documented Random number generators"
    This reverts commit d101883dcc1ce03c35c188fd3aab63230140bb40.

--- include/entities/RandomGenerator_t.h
+++ include/entities/RandomGenerator_t.h
@@ -5,22 +5,8 @@
 #include <vector>
 
 namespace APTracer { namespace Entities {
-    extern std::vector<std::mt19937> rng_vec; /**< @brief Global vector of random generators initialised once per thread and used through the program.*/
-
-    /**
-     * @brief Initialises the array of random generators, one for each thread.
-     * 
-     * This is called sometimes before main, when rng_vec is initialised in the global namespace. 
-     * 
-     * @return std::vector<std::mt19937> 
-     */
+    extern std::vector<std::mt19937> rng_vec;
     std::vector<std::mt19937> build_rng_vec();
-
-    /**
-     * @brief Returns the thread's random generator within rng_vec.
-     * 
-     * @return std::mt19937& Reference to this thread's random generator.
-     */
     std::mt19937& rng(); /**< @brief Global random generator initialised once per thread and used through the program.*/
 }}
 #endif
