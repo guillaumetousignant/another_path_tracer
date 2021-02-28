4681ccff3d24cf97e45dd8931787e8cbf53a28ad
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-02-28 15:13:50 -0500

    documented portal scatterers

--- include/materials/PortalScattererTop_t.h
+++ include/materials/PortalScattererTop_t.h
@@ -12,13 +12,32 @@ using APTracer::Entities::Medium_t;
 using APTracer::Entities::TransformMatrix_t;
 
 namespace APTracer { namespace Materials {
+
+    /**
+     * @brief The portal scatterer top class is an interface for portal mediums, defining the base they need to move rays around.
+     * 
+     * This class contains a transformation matrix, which determines how rays are transformed when scattered by the portal.
+     * It also contains a medium list, to determine in which mediums are the rays after being transformed.
+     */
     class PortalScattererTop_t : public Medium_t{
         public:
-            PortalScattererTop_t(TransformMatrix_t* transformation, std::list<Medium_t*> medium_lis, double ind, unsigned int priority);
+            /**
+             * @brief Construct a new PortalScattererTop_t object with a transformation matrix and a medium list.
+             * 
+             * @param transformation Transformation matrix which transforms the rays on hit, potentially moving them to another location. 
+             * @param medium_list List of materials in which the portal end is placed. Should have at least two copies of an "outside" medium not assigned to any object (issue #25).
+             * @param ind Refractive index of the medium.
+             * @param priority Priority of the medium over other mediums, used to determine which is the current medium when overlapping. Higher value means higher priority.
+             */
+            PortalScattererTop_t(TransformMatrix_t* transformation, std::list<Medium_t*> medium_list, double ind, unsigned int priority);
+
+            /**
+             * @brief Destroy the PortalScattererTop_t object. Does nothing.
+             */
             virtual ~PortalScattererTop_t();
 
-            TransformMatrix_t* transformation_;
-            std::list<Medium_t*> medium_list_;
+            TransformMatrix_t* transformation_; /**< @brief Transformation matrix which transforms the rays when scattered, potentially moving them to another location.*/
+            std::list<Medium_t*> medium_list_; /**< @brief List of materials in which the portal end is placed. Should have at least two copies of an "outside" medium not assigned to any object (issue #25).*/
     };
 }}
 #endif
--- src/materials/PortalScatterer_t.cpp
+++ src/materials/PortalScatterer_t.cpp
@@ -2,7 +2,7 @@
 #include "entities/TransformMatrix_t.h"
 #include "entities/RandomGenerator_t.h"
 
-APTracer::Materials::PortalScatterer_t::PortalScatterer_t(APTracer::Entities::TransformMatrix_t* transformation, double scattering_distance, std::list<APTracer::Entities::Medium_t*> medium_list, double ind, unsigned int priority)
+APTracer::Materials::PortalScatterer_t::PortalScatterer_t(APTracer::Entities::TransformMatrix_t* transformation, std::list<APTracer::Entities::Medium_t*> medium_list, double scattering_distance, double ind, unsigned int priority)
     : PortalScattererTop_t(transformation, medium_list, ind, priority), scattering_coefficient_(1/scattering_distance), unif_(0.0, 1.0) {}
 
 APTracer::Materials::PortalScatterer_t::~PortalScatterer_t() = default;
