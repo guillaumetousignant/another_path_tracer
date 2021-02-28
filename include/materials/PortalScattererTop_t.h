43b3edc0cfbd4d70f21d3a108900c8e3d3e42ca8
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-02-28 14:47:22 -0500

    typo

--- include/materials/PortalScattererTop_t.h
+++ include/materials/PortalScattererTop_t.h
@@ -14,7 +14,7 @@ using APTracer::Entities::TransformMatrix_t;
 namespace APTracer { namespace Materials {
     class PortalScattererTop_t : public Medium_t{
         public:
-            PortalScattererTop_t(TransformMatrix_t* transformation, std::list<Medium_t*> medium_lis, double ind, unsigned int priorityt);
+            PortalScattererTop_t(TransformMatrix_t* transformation, std::list<Medium_t*> medium_lis, double ind, unsigned int priority);
             virtual ~PortalScattererTop_t();
 
             TransformMatrix_t* transformation_;
