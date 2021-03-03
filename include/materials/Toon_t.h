f1ba840a76f160ba7a76da192f21121fbb4c4736
Guillaume Tousignant <moi_guillaume@hotmail.com>
2021-03-03 13:30:50 -0500

    documented toon and transparent materials

--- include/materials/Toon_t.h
+++ include/materials/Toon_t.h
@@ -15,13 +15,40 @@ using APTracer::Entities::Ray_t;
 using APTracer::Entities::Shape_t;
 
 namespace APTracer { namespace Materials {
+
+    /**
+     * @brief The toon class describes a material that has a solid colour, regardless of lighting.
+     * 
+     * This material sets rays' colour to its colour, and stops them bouncing.
+     * This material isn't affected by lights and shadows, or any lighting.
+     * This material is more useful for debugging, to force a specific colour, 
+     * to create impossible lighting and for toon-like shading.
+     */
     class Toon_t final : public Material_t{
         public:
+            /**
+             * @brief Construct a new Toon_t object with a colour.
+             * 
+             * @param colour Colour of the material, regardless of shading.
+             */
             Toon_t(const Vec3f &colour);
+
+            /**
+             * @brief Destroy the Toon_t object. Does nothing.
+             */
             virtual ~Toon_t() final;
 
-            Vec3f colour_;
+            Vec3f colour_; /**< @brief Colour of the material, regardless of shading.*/
 
+            /**
+             * @brief Bounces a ray of light on the material.
+             * 
+             * This sets the ray's colour to the material's colour, and sets the ray's mask to 0 to prevent further bouncing.
+             * 
+             * @param uv Object space coordinates of the hit point. Used to query the shape for values at coordinates on it. Two components, u, and v, that can change meaning depending on the shape.
+             * @param hit_obj Pointer to the shape that was hit by the ray.
+             * @param ray Ray that has intersected the shape.
+             */
             virtual void bounce(std::array<double, 2> uv, const Shape_t* hit_obj, Ray_t &ray) final;
     };
 }}
