c2833e0d3324a1a4a5f62d8893c798db2565a639
Guillaume Tousignant <moi_guillaume@hotmail.com>
2025-01-19 10:07:17 -0500

    Scatterer colour and emission were switched

--- src/materials/Scatterer_t.cpp
+++ src/materials/Scatterer_t.cpp
@@ -8,8 +8,8 @@ using APTracer::Entities::Vec3f;
 
 APTracer::Materials::Scatterer_t::Scatterer_t(Entities::Vec3f emi_vol, Entities::Vec3f col_vol, double abs_dist_emi, double abs_dist_col, double scat_dist, double ind, unsigned int priority) :
         Medium_t(ind, priority),
-        emission_vol_(-col_vol.ln() / abs_dist_col),
-        colour_vol_(emi_vol * emi_vol / abs_dist_emi /*CHECK probably not right.*/),
+        emission_vol_(emi_vol * emi_vol / abs_dist_emi /*CHECK probably not right.*/),
+        colour_vol_(-col_vol.ln() / abs_dist_col),
         scattering_coefficient_(1.0 / scat_dist),
         unif_(0.0, 1.0) {}
 
